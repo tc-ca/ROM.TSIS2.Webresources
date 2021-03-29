b9a4f3eeda3703c8269c1ef0390ed23cdc35e96e
Jason Au <jason@infusionws.com>
2021-03-29 11:30:13 -0400

    Using current datetime for In Progress bookings

--- TSIS2.WebResources/Webresources/js/BookableResourceBooking.js
+++ TSIS2.WebResources/Webresources/js/BookableResourceBooking.js
@@ -5,11 +5,78 @@ var ROM;
     var BookableResourceBooking;
     (function (BookableResourceBooking) {
         // EVENTS
-        function starttimeOnChange(eContext) {
+        function bookingStatusOnChange(eContext) {
+            var form = eContext.getFormContext();
+            var bookingStatusAttribute = form.getAttribute("bookingstatus");
+            var actualArrivalTimeAttribute = form.getAttribute("msdyn_actualarrivaltime");
+            // Only do this if arrival time hasn't been set before
+            if (actualArrivalTimeAttribute != null || actualArrivalTimeAttribute != undefined) {
+                var actualArrivalTimeAttributeValue = actualArrivalTimeAttribute.getValue();
+                if (actualArrivalTimeAttributeValue == null || actualArrivalTimeAttributeValue == undefined) {
+                    // Only do this if the booking status changes to "In Progress"
+                    if (bookingStatusAttribute != null && bookingStatusAttribute != undefined) {
+                        var bookingStatusAttributeValue = bookingStatusAttribute.getValue();
+                        if (bookingStatusAttributeValue != null && bookingStatusAttributeValue != undefined) {
+                            if (bookingStatusAttributeValue[0].name == Xrm.Utility.getResourceString("ovs_/resx/BookableResourceBooking", "InProgressBookingStatus")) {
+                                // Get the client's current date time
+                                //NOTE: This already takes into account timezone offset
+                                var currentDateTime_1 = new Date();
+                                var startTimeAttribute_1 = form.getAttribute("starttime");
+                                var endTimeAttribute_1 = form.getAttribute("endtime");
+                                var beforeStartTime_1 = false;
+                                var afterEndTime_1 = false;
+                                if (startTimeAttribute_1 != null && startTimeAttribute_1 != undefined) {
+                                    var startTimeAttributeValue = startTimeAttribute_1.getValue();
+                                    if (startTimeAttributeValue != null && startTimeAttributeValue != undefined) {
+                                        var startTimeDiff = startTimeAttributeValue.getTime() - currentDateTime_1.getTime();
+                                        var startTimeDiffMins = Math.round(((startTimeDiff % 86400000) % 3600000) / 60000);
+                                        if (startTimeDiffMins > 0) {
+                                            beforeStartTime_1 = true;
+                                        }
+                                    }
+                                }
+                                if (endTimeAttribute_1 != null && endTimeAttribute_1 != undefined) {
+                                    var endTimeAttributeValue = endTimeAttribute_1.getValue();
+                                    if (endTimeAttributeValue != null && endTimeAttributeValue != undefined) {
+                                        var endTimeDiff = endTimeAttributeValue.getTime() - currentDateTime_1.getTime();
+                                        var endTimeDiffMins = Math.round(((endTimeDiff % 86400000) % 3600000) / 60000);
+                                        if (endTimeDiffMins < 0) {
+                                            afterEndTime_1 = true;
+                                        }
+                                    }
+                                }
+                                // Ask the client if they want to re-adjust the start and end datetime if they conflict with the current datetime
+                                if (beforeStartTime_1 || afterEndTime_1) {
+                                    var confirmStrings = {
+                                        text: Xrm.Utility.getResourceString("ovs_/resx/BookableResourceBooking", "ConfirmStartEndTimeAdjustmentText"),
+                                        title: Xrm.Utility.getResourceString("ovs_/resx/BookableResourceBooking", "ConfirmStartEndTimeAdjustmentTitle")
+                                    };
+                                    var confirmOptions = { height: 200, width: 450 };
+                                    Xrm.Navigation.openConfirmDialog(confirmStrings, confirmOptions).then(function (success) {
+                                        if (success.confirmed) {
+                                            if (beforeStartTime_1) {
+                                                startTimeAttribute_1.setValue(currentDateTime_1);
+                                                // Update the estimated arrival time too
+                                                form.getAttribute("msdyn_estimatedarrivaltime").setValue(currentDateTime_1);
+                                            }
+                                            if (afterEndTime_1) {
+                                                endTimeAttribute_1.setValue(currentDateTime_1);
+                                            }
+                                        }
+                                        // Set the actual arrival time to the client's current date time when booking status is set to "In Progress"
+                                        form.getAttribute("msdyn_actualarrivaltime").setValue(currentDateTime_1);
+                                    });
+                                }
+                                else {
+                                    // Set the actual arrival time to the client's current date time when booking status is set to "In Progress"
+                                    form.getAttribute("msdyn_actualarrivaltime").setValue(currentDateTime_1);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-        BookableResourceBooking.starttimeOnChange = starttimeOnChange;
-        function endtimeOnChange(eContext) {
-        }
-        BookableResourceBooking.endtimeOnChange = endtimeOnChange;
+        BookableResourceBooking.bookingStatusOnChange = bookingStatusOnChange;
     })(BookableResourceBooking = ROM.BookableResourceBooking || (ROM.BookableResourceBooking = {}));
 })(ROM || (ROM = {}));
--- TSIS2.WebResources/src/ts/BookableResourceBooking.ts
+++ TSIS2.WebResources/src/ts/BookableResourceBooking.ts
@@ -1,9 +1,89 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 namespace ROM.BookableResourceBooking {
     // EVENTS
-    export function starttimeOnChange(eContext: Xrm.ExecutionContext<any, any>): void {
-    }
+    export function bookingStatusOnChange(eContext: Xrm.ExecutionContext<any, any>): void {
+        const form = <Form.bookableresourcebooking.Main.ROMInformation>eContext.getFormContext();
+        const bookingStatusAttribute = form.getAttribute("bookingstatus");    
+        const actualArrivalTimeAttribute = form.getAttribute("msdyn_actualarrivaltime");
 
-    export function endtimeOnChange(eContext: Xrm.ExecutionContext<any, any>): void {
+        // Only do this if arrival time hasn't been set before
+        if (actualArrivalTimeAttribute != null || actualArrivalTimeAttribute != undefined) {
+            const actualArrivalTimeAttributeValue = actualArrivalTimeAttribute.getValue();
+            if (actualArrivalTimeAttributeValue == null || actualArrivalTimeAttributeValue == undefined) {
+
+                // Only do this if the booking status changes to "In Progress"
+                if (bookingStatusAttribute != null && bookingStatusAttribute != undefined) {
+                    const bookingStatusAttributeValue = bookingStatusAttribute.getValue();
+
+                    if (bookingStatusAttributeValue != null && bookingStatusAttributeValue != undefined) {
+                        if (bookingStatusAttributeValue[0].name == Xrm.Utility.getResourceString("ovs_/resx/BookableResourceBooking", "InProgressBookingStatus")) {
+
+                            // Get the client's current date time
+                            //NOTE: This already takes into account timezone offset
+                            const currentDateTime = new Date();
+
+                            const startTimeAttribute = form.getAttribute("starttime");
+                            const endTimeAttribute = form.getAttribute("endtime");
+
+                            let beforeStartTime = false;
+                            let afterEndTime = false;
+
+                            if (startTimeAttribute != null && startTimeAttribute != undefined) {
+                                const startTimeAttributeValue = startTimeAttribute.getValue();
+                                if (startTimeAttributeValue != null && startTimeAttributeValue != undefined) {
+                                    const startTimeDiff = startTimeAttributeValue.getTime() - currentDateTime.getTime();
+                                    const startTimeDiffMins = Math.round(((startTimeDiff % 86400000) % 3600000) / 60000);
+                                    if (startTimeDiffMins > 0) {
+                                        beforeStartTime = true;
+                                    }
+                                }
+                            }
+
+                            if (endTimeAttribute != null && endTimeAttribute != undefined) {
+                                const endTimeAttributeValue = endTimeAttribute.getValue();
+                                if (endTimeAttributeValue != null && endTimeAttributeValue != undefined) {
+                                    const endTimeDiff = endTimeAttributeValue.getTime() - currentDateTime.getTime();
+                                    const endTimeDiffMins = Math.round(((endTimeDiff % 86400000) % 3600000) / 60000);
+                                    if (endTimeDiffMins < 0) {
+                                        afterEndTime = true;
+                                    }
+                                }
+                            }
+
+                            // Ask the client if they want to re-adjust the start and end datetime if they conflict with the current datetime
+                            if (beforeStartTime || afterEndTime) {
+
+                                const confirmStrings = {
+                                    text: Xrm.Utility.getResourceString("ovs_/resx/BookableResourceBooking", "ConfirmStartEndTimeAdjustmentText"),
+                                    title: Xrm.Utility.getResourceString("ovs_/resx/BookableResourceBooking", "ConfirmStartEndTimeAdjustmentTitle")
+                                };
+                                const confirmOptions = { height: 200, width: 450 };
+                                Xrm.Navigation.openConfirmDialog(confirmStrings, confirmOptions).then(
+                                    function (success) {
+                                        if (success.confirmed) {
+                                            if (beforeStartTime) {
+                                                startTimeAttribute.setValue(currentDateTime);
+                                                // Update the estimated arrival time too
+                                                form.getAttribute("msdyn_estimatedarrivaltime").setValue(currentDateTime);
+                                            }
+                                            if (afterEndTime) {
+                                                endTimeAttribute.setValue(currentDateTime);
+                                            }
+                                        }
+                                        // Set the actual arrival time to the client's current date time when booking status is set to "In Progress"
+                                        form.getAttribute("msdyn_actualarrivaltime").setValue(currentDateTime);
+                                    }
+                                );
+
+                            } else {
+                                // Set the actual arrival time to the client's current date time when booking status is set to "In Progress"
+                                form.getAttribute("msdyn_actualarrivaltime").setValue(currentDateTime);
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
     }
 }
