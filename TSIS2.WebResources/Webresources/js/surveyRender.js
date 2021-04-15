a400b7db8d3ba9650da862d603abd8375f7777a2
DBurneyTC <Derek.Burney@tc.gc.ca>
2021-04-15 15:31:30 -0400

    Maintain survey page number after save

--- TSIS2.WebResources/Webresources/js/surveyRender.js
+++ TSIS2.WebResources/Webresources/js/surveyRender.js
@@ -109,5 +109,7 @@ function InitializeSurveyRender(surveyDefinition, surveyResponse, surveyLocale, 
 }
 
 function DoComplete() {
+    var currentPageNo = survey.currentPageNo;
     window.survey.doComplete();
+    survey.currentPage = currentPageNo;
 } 
