0cddd5701028d217c503aadf2ef89bfdef6a4b4e
H <hamza.etth@gmail.com>
2021-04-21 14:04:18 -0400

    Removed unneeded behavior in onLoad of Work Order Quick Create Form

--- TSIS2.WebResources/src/ts/WorkOrderQuickCreate.ts
+++ TSIS2.WebResources/src/ts/WorkOrderQuickCreate.ts
@@ -3,13 +3,6 @@ namespace ROM.WorkOrderQuickCreate {
     // EVENTS
     export function onLoad(eContext: Xrm.ExecutionContext<any, any>): void {
         const form = <Form.msdyn_workorder.QuickCreate.QuickCreateWorkOrder>eContext.getFormContext();
-
-        switch (form.ui.getFormType()) {
-            //Create
-            case 1:
-                setRegion(eContext);
-            break;
-        }
     }
 
     export function regionOnChange(eContext: Xrm.ExecutionContext<any, any>): void {
@@ -36,53 +29,4 @@ namespace ROM.WorkOrderQuickCreate {
             throw new Error(e.Message);
         }
     }
-
-    // FUNCTIONS
-    function setRegion(eContext: Xrm.ExecutionContext<any, any>): void {
-        var currentUserId = Xrm.Utility.getGlobalContext().userSettings.userId;
-        currentUserId = currentUserId.replace(/[{}]/g, "");
-
-        // Get the user's territory
-        Xrm.WebApi.online.retrieveRecord("systemuser", currentUserId, "?$select=_territoryid_value").then(
-            function success(result) {
-                const form = <Form.msdyn_workorder.QuickCreate.QuickCreateWorkOrder>eContext.getFormContext();
-                if (result != null && result["_territoryid_value"] != null) {
-
-                    // NOTE: Our localization plugin can't localize the territory name on system user
-                    // So we do an extra call to the territory table to get the localized name
-                    Xrm.WebApi.online.retrieveRecord("territory", result["_territoryid_value"], "?$select=name").then(
-                        function success(result) {
-                            const territoryId = result["territoryid"];
-                            var territoryName = result["name"];
-                            var territoryLogicalName = "territory";
-                            var lookup = new Array();
-                            lookup[0] = new Object();
-                            lookup[0].id = territoryId;
-                            lookup[0].name = territoryName;
-                            lookup[0].entityType = territoryLogicalName;
-                            form.getAttribute('msdyn_serviceterritory').setValue(lookup);
-                            if(lookup[0].name == "International"){
-                                form.getControl("ovs_ovscountry").setVisible(true);
-                            }
-                            else{
-                                regionOnChange(eContext);
-                            }
-                        },
-                        function (error) {
-                            var alertStrings = { text: error.message };
-                            var alertOptions = { height: 120, width: 260 };
-                            Xrm.Navigation.openAlertDialog(alertStrings, alertOptions).then(function () { });
-                        }
-                    );
-
-                }
-            },
-            function (error) {
-                var alertStrings = { text: error.message };
-                var alertOptions = { height: 120, width: 260 };
-                Xrm.Navigation.openAlertDialog(alertStrings, alertOptions).then(function () { });
-            }
-        );
-    }
-
   }
