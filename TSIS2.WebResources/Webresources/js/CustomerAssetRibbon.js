275331e24d831c242afd2289e1f25e93d8c4dd2f
H <hamza.etth@gmail.com>
2021-05-14 13:52:58 -0400

    Fix adding Assets to WorkOrder

--- TSIS2.WebResources/Webresources/js/CustomerAssetRibbon.js
+++ TSIS2.WebResources/Webresources/js/CustomerAssetRibbon.js
@@ -1,7 +1,31 @@
 function addExistingAssetsToWorkOrder(primaryControl, selectedEntityTypeName, selectedControl){
     const formContext = primaryControl;
-
     const workOrderID = Xrm.Page.data.entity.getId().replace(/({|})/g,'');
+    var customerAssetsAlreadyAssociatedCondition = "";
+
+
+
+    var req = new XMLHttpRequest();
+    req.open("GET", formContext.context.getClientUrl() + "/api/data/v9.0/" + "msdyn_workorders" + "(" + workOrderID+ ")" + "/" + "ts_msdyn_customerasset_msdyn_workorder_msdyn", false);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader("Accept", "application/json");
+    req.setRequestHeader("OData-MaxVersion", "4.0");
+    req.setRequestHeader("OData-Version", "4.0");
+
+    req.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            req.onreadystatechange = null;
+            if (this.status === 200) {
+                var results = JSON.parse(this.response);
+                for (var i = 0; i < results.value.length; i++) {
+                    customerAssetsAlreadyAssociatedCondition += `<condition attribute="msdyn_customerassetid" operator="neq" value="${results.value[i]["msdyn_customerassetid"]}" />`;
+                }
+            } else {
+                Xrm.Utility.alertDialog(this.statusText);
+            }
+        }
+    };
+    req.send();
 
     var lookupOptions =
     {
@@ -10,13 +34,23 @@ function addExistingAssetsToWorkOrder(primaryControl, selectedEntityTypeName, se
         allowMultiSelect: true,
         defaultViewId:"bf49a9fc-82a7-eb11-9442-000d3a8410dc",
         disableMru: true,
+        filters: [
+            {
+                filterXml: `<filter type="and">` + 
+                    `${customerAssetsAlreadyAssociatedCondition}` +
+                    `</filter> `,
+                entityLogicalName: "msdyn_customerasset"
+            }
+        ]
     };
+
+    console.log(customerAssetsAlreadyAssociatedCondition)
 
     Xrm.Utility.lookupObjects(lookupOptions).then(
     function(result){
         console.log(result);
         for (var i = 0; i < result.length; i++) {
-            var req = new XMLHttpRequest();
+            req = new XMLHttpRequest();
 
             req.open("POST", formContext.context.getClientUrl() + "/api/data/v9.0/" + "msdyn_customerassets" + "(" + result[i].id.replace(/({|})/g,'') + ")" + "/" + "ts_msdyn_customerasset_msdyn_workorder_msdyn" + "/$ref");
             req.setRequestHeader("Content-Type", "application/json");
@@ -28,9 +62,22 @@ function addExistingAssetsToWorkOrder(primaryControl, selectedEntityTypeName, se
                 {
                     "@odata.id" : formContext.context.getClientUrl() + "/api/data/v9.0/" + "msdyn_workorders" + "(" + workOrderID + ")"
                 };
+            
+            req.onreadystatechange = function() {
+                if (this.readyState === 4) {
+                    req.onreadystatechange = null;
+                    if (this.status === 204) {
+                        selectedControl.refresh();
+                    } else {
+                        showErrorMessageAlert(this.statusText);
+                    }
+                }
+            };
+    
 
             req.send(JSON.stringify(payload));
         }
+
     },
     function (error){
         showErrorMessageAlert(error);
