8da3e399fa6d6ab6a4f7d08108956f53a64e008d
DBurneyTC <Derek.Burney@tc.gc.ca>
2021-05-18 16:23:50 -0400

    remove old printing code

--- TSIS2.WebResources/Webresources/js/surveyRender.js
+++ TSIS2.WebResources/Webresources/js/surveyRender.js
@@ -65,29 +65,6 @@ function InitializeSurveyRender(surveyDefinition, surveyResponse, surveyLocale, 
         survey.render();
     });
 
-    /*
-    survey.onAfterRenderPage.add(function (survey, options) {
-        var printSurveyButton = document.createElement("button");
-        printSurveyButton.type = "button";
-        printSurveyButton.className = "btn btn-primary sv-btn svd-toolbar-button";
-        printSurveyButton.innerHTML = "Print Survey";
-        printSurveyButton.onclick = function () {
-            var wrCtrlPrint = parentFormContext.getControl('WebResource_SurveyRenderPrint');
-            wrCtrlPrint.getContentWindow().then(function (win) {
-                var printContents = win.document.getElementById("printableSurvey").innerHTML;
-                var originalContents = document.body.innerHTML;
-                document.body.innerHTML = printContents;
-                window.print();
-                document.body.innerHTML = originalContents;
-            });
-        }
-        var header = options
-            .htmlElement
-            .querySelector("h4");
-        header.appendChild(printSurveyButton);
-    });
-    */
-
     survey.onAfterRenderSurvey.add(function (survey, options) {
         // Hide complete button after survey renders.
         $('.sv_complete_btn').hide();
