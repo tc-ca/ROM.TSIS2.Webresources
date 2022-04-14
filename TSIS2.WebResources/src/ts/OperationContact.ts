cb96a0f0faf385b116111b3e7a1c532d14c2d486
H <hamza.etth@gmail.com>
2022-04-14 12:42:10 -0400

    Missing %

--- TSIS2.WebResources/src/ts/OperationContact.ts
+++ TSIS2.WebResources/src/ts/OperationContact.ts
@@ -22,12 +22,12 @@ namespace ROM.OperationContact {
             let userBusinessUnitName = result.entities[0].name;
 
             //Only show AvSec/ISSO Operations in same BU
-            if(!userBusinessUnitName.startsWith("Transport")){
+            if(!(userBusinessUnitName.startsWith("Transport"))){
                 const viewIdActivity = '{D8BE9759-2765-402C-8AA5-6786738AD020}';
                 const entityNameActivity = "ovs_operation";
                 const viewDisplayNameActivity = "Filtered Operations"
                 const layoutXmlActivity = '<grid name="resultset" object="10010" jump="ovs_name" select="1" icon="1" preview="1"><row name="result" id="ovs_operationid"><cell name="ovs_name" width="200" /></row></grid>';
-                const fetchXmlActivity = '<fetch version="1.0" mapping="logical"><entity name="ovs_operation"><filter type="and"><condition attribute="statecode" operator="eq" value="0"/></filter><attribute name="ovs_operationid"/><attribute name="ovs_name"/><order attribute="ovs_name"/><link-entity name="businessunit" from="businessunitid" to="owningbusinessunit"><attribute name="name"/><filter><condition attribute="name" operator="like" value="' + userBusinessUnitName.substring(0,4) + '"/></filter></link-entity></entity></fetch>';
+                const fetchXmlActivity = '<fetch version="1.0" mapping="logical"><entity name="ovs_operation"><filter type="and"><condition attribute="statecode" operator="eq" value="0"/></filter><attribute name="ovs_operationid"/><attribute name="ovs_name"/><order attribute="ovs_name"/><link-entity name="businessunit" from="businessunitid" to="owningbusinessunit"><attribute name="name"/><filter><condition attribute="name" operator="like" value="' + userBusinessUnitName.substring(0,5) + '%"/></filter></link-entity></entity></fetch>';
     
                 form.getControl("ts_operation").addCustomView(viewIdActivity, entityNameActivity, viewDisplayNameActivity, fetchXmlActivity, layoutXmlActivity, true);            }           
         });        
