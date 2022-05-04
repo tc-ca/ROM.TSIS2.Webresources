b5b0983d6fb1ccae8b8a229ad52e27550eac3796
Dong Hua <dong.hua@tc.gc.ca>
2022-05-04 10:54:42 -0400

    Fixed issue for 169287, added details / fields into operation lookup field

--- TSIS2.WebResources/Webresources/js/OperationContact.js
+++ TSIS2.WebResources/Webresources/js/OperationContact.js
@@ -28,8 +28,8 @@ var ROM;
                     var viewIdActivity = '{D8BE9759-2765-402C-8AA5-6786738AD020}';
                     var entityNameActivity = "ovs_operation";
                     var viewDisplayNameActivity = "Filtered Operations";
-                    var layoutXmlActivity = '<grid name="resultset" object="10010" jump="ovs_name" select="1" icon="1" preview="1"><row name="result" id="ovs_operationid"><cell name="ovs_name" width="200" /></row></grid>';
-                    var fetchXmlActivity = '<fetch version="1.0" mapping="logical"><entity name="ovs_operation"><filter type="and"><condition attribute="statecode" operator="eq" value="0"/></filter><attribute name="ovs_operationid"/><attribute name="ovs_name"/><order attribute="ovs_name"/><link-entity name="businessunit" from="businessunitid" to="owningbusinessunit"><attribute name="name"/><filter><condition attribute="name" operator="like" value="' + userBusinessUnitName.substring(0, 5) + '%"/></filter></link-entity></entity></fetch>';
+                    var layoutXmlActivity = '<grid name="resultset" object="10010" jump="ovs_name" select="1" icon="1" preview="1"><row name="result" id="ovs_operationid"><cell name="ovs_name" width="200" /><cell name="ts_site" width="200" /><cell name="ovs_operationtypeid" width="200" /></row></grid>';
+                    var fetchXmlActivity = '<fetch version="1.0" mapping="logical"><entity name="ovs_operation"><filter type="and"><condition attribute="statecode" operator="eq" value="0"/></filter><attribute name="ovs_operationid"/><attribute name="ovs_name"/><attribute name="ts_site"/><attribute name="ovs_operationtypeid"/><order attribute="ovs_name"/><link-entity name="businessunit" from="businessunitid" to="owningbusinessunit"><attribute name="name"/><filter><condition attribute="name" operator="like" value="' + userBusinessUnitName.substring(0, 5) + '%"/></filter></link-entity></entity></fetch>';
                     form.getControl("ts_operation").addCustomView(viewIdActivity, entityNameActivity, viewDisplayNameActivity, fetchXmlActivity, layoutXmlActivity, true);
                 }
             });
