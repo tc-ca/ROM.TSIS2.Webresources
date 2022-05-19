fc50c1624279d60ab024bc1c8fc3281ffda813bb
H <hamza.etth@gmail.com>
2022-05-19 14:32:19 -0400

    Typo fix

--- TSIS2.WebResources/Webresources/js/Email.js
+++ TSIS2.WebResources/Webresources/js/Email.js
@@ -68,7 +68,7 @@ var ROM;
                     formContext.getAttribute("to").setValue(contact);
                 }
             }
-            //Fill regarding to field
+            //Fill regarding field
             //@ts-ignore
             if (formContext.data.attributes.get("regardingobjectid_0").getValue() != null && formContext.data.attributes.get("regardingobjectname_0").getValue() != null) {
                 //Retrieve custom parameters sent from WorkOrderServiceTaskRibbon to fill the "regardingobjectid" lookup field"
