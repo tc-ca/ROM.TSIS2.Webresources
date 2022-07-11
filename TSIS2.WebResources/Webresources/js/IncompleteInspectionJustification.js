00d28fb692344c2180154dcb9e553e1649fd4cb2
Petrie, Robert <robert.petrie@tc.gc.ca>
2022-07-11 15:41:50 -0400

    Boilerplate code for Incomplete Inspection Justification

---
+++ TSIS2.WebResources/Webresources/js/IncompleteInspectionJustification.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+var ROM;
+(function (ROM) {
+    var IncompleteInspectionJustification;
+    (function (IncompleteInspectionJustification) {
+        function onLoad(eContext) {
+        }
+        IncompleteInspectionJustification.onLoad = onLoad;
+        function onSave(eContext) {
+        }
+        IncompleteInspectionJustification.onSave = onSave;
+    })(IncompleteInspectionJustification = ROM.IncompleteInspectionJustification || (ROM.IncompleteInspectionJustification = {}));
+})(ROM || (ROM = {}));
---
+++ TSIS2.WebResources/src/ts/IncompleteInspectionJustification.ts
@@ -0,0 +1,8 @@
+﻿namespace ROM.IncompleteInspectionJustification {
+
+    export function onLoad(eContext: Xrm.ExecutionContext<any, any>): void {
+    }
+
+    export function onSave(eContext: Xrm.ExecutionContext<any, any>): void {
+    }
+}
