1db6bc79df39f60f82e99c319cd325b1faa498d7
DBurneyTC <Derek.Burney@tc.gc.ca>
2022-09-09 16:03:01 -0400

    Add Commit Trip Ribbon function

--- TSIS2.WebResources/Webresources/js/TripRibbon.js
+++ TSIS2.WebResources/Webresources/js/TripRibbon.js
@@ -16,3 +16,34 @@ function planningTrip(data) {
         }
     });
 }
+
+function commitRelatedWorkOrdersOfSelectedTrips(selectedTripsGuids, selectedControl) {
+    for (let tripGuid of selectedTripsGuids) {
+        var fetchXml = [
+            "<fetch>",
+            "  <entity name='ts_tripinspection'>",
+            "    <attribute name='ts_inspection' />",
+            "    <filter>",
+            "      <condition attribute='ts_trip' operator='eq' value='", tripGuid, "'/>",
+            "    </filter>",
+            "  </entity>",
+            "</fetch>",
+        ].join("");
+        fetchXml = "?fetchXml=" + encodeURIComponent(fetchXml);
+        Xrm.WebApi.retrieveMultipleRecords("ts_tripinspection", fetchXml).then(
+            function success(result) {
+                for (var i = 0; i < result.entities.length; i++) {
+                    const workOrderGuid = result.entities[i]._ts_inspection_value;
+                    Xrm.WebApi.updateRecord("msdyn_workorder", workOrderGuid, { "ts_state": 717750001 });
+                }
+                var alertStrings = { confirmButtonLabel: "OK", text: "The Work Orders related to the selected Trip(s) have been set to a Committed State", title: "Work Orders Committed" };
+                var alertOptions = { height: 200, width: 200 };
+                Xrm.Navigation.openAlertDialog(alertStrings, alertOptions).then(
+                    function success(result) {
+                        selectedControl.refresh();
+                    }
+                );
+            }
+        );
+    }
+}
