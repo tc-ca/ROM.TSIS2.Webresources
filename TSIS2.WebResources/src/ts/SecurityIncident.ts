80a80713599154bbdc328cb6d8a4dcabcc8d7f92
Dong Hua <dong.hua@tc.gc.ca>
2022-11-21 15:01:02 -0500

    PBI AB#177219- 'Who' Fields - Incidents

--- TSIS2.WebResources/src/ts/SecurityIncident.ts
+++ TSIS2.WebResources/src/ts/SecurityIncident.ts
@@ -1,4 +1,16 @@
 namespace ROM.SecurityIncident {
     export function onLoad(eContext: Xrm.ExecutionContext<any, any>): void {
+        const formContext = <Form.ts_securityincident.Main.Information>eContext.getFormContext();
+        if (formContext.ui.getFormType() == 2) {
+            StatusOfRailwayOwnerOnChange(eContext);
+        }
+    }
+    export function StatusOfRailwayOwnerOnChange(eContext: Xrm.ExecutionContext<any, any>): void {
+        let formContext = <Form.ts_securityincident.Main.Information>eContext.getFormContext();
+        let statusOfRailwayOwner = formContext.getAttribute("ts_statusofrailwayowner").getValue();
+        if (statusOfRailwayOwner == null || (statusOfRailwayOwner != null && statusOfRailwayOwner == ts_statusofrailwayowner.Known))
+            formContext.getControl("ts_owneroftherailwaylinetrack").setVisible(true);
+        else
+            formContext.getControl("ts_owneroftherailwaylinetrack").setVisible(false);
     }
 }
