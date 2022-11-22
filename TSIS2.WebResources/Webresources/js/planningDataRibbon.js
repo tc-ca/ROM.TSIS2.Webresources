e6eb1f34c96f830b48e2af622e9972187c8c516e
DBurneyTC <Derek.Burney@tc.gc.ca>
2022-11-22 12:07:55 -0500

    add async and await

--- TSIS2.WebResources/Webresources/js/planningDataRibbon.js
+++ TSIS2.WebResources/Webresources/js/planningDataRibbon.js
@@ -1,13 +1,13 @@
-﻿function setSelectedNonOperational(planningDataGuids, formContext) {
+﻿function setSelectedNonOperational(planningDataGuids, primaryControl) {
     var confirmStrings = { text: "The Operation Activities related to the selected records will become Non-Operational. Do you wish to proceed?", title: "Set to Non-Operational", confirmButtonLabel: "Yes", cancelButtonLabel: "Cancel" };
     var confirmOptions = { height: 200, width: 450 };
     Xrm.Navigation.openConfirmDialog(confirmStrings, confirmOptions).then(
         async function (success) {
             if (success.confirmed) {
                 Xrm.Utility.showProgressIndicator();
-                let gridControl = formContext.getControl("subgrid_planning_data");
+                let gridControl = primaryControl.getControl("subgrid_planning_data");
                 for (let planningDataGuid of planningDataGuids) {
-                    Xrm.WebApi.retrieveRecord("ts_planningdata", planningDataGuid, "?$select=_ts_operationactivity_value").then(function (result) {
+                    await Xrm.WebApi.retrieveRecord("ts_planningdata", planningDataGuid, "?$select=_ts_operationactivity_value").then(async function (result) {
                         var data =
                         {
                             "ts_operationalstatus": 717750001,
@@ -18,5 +18,6 @@
                 setTimeout(gridControl.refresh(), 1000);
                 Xrm.Utility.closeProgressIndicator();
             }
-        });
+        }
+);
 }
