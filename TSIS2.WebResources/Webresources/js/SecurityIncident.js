3fe1baa4e22bf53d67bf45632b0e617d56a9ec8f
H <hamza.etth@gmail.com>
2022-11-22 10:19:31 -0500

    Visibility logic for Security Incident

--- TSIS2.WebResources/Webresources/js/SecurityIncident.js
+++ TSIS2.WebResources/Webresources/js/SecurityIncident.js
@@ -4,7 +4,32 @@ var ROM;
     var SecurityIncident;
     (function (SecurityIncident) {
         function onLoad(eContext) {
+            var form = eContext.getFormContext();
+            if (form.getAttribute("ts_delaystooperation").getValue() == 717750000 /* Known */) {
+                form.getControl("ts_delaystooperationtime").setVisible(true);
+            }
+            if (form.getAttribute("ts_arrests").getValue() == 717750000 /* Known */) {
+                form.getControl("ts_arrestscount").setVisible(true);
+                form.getControl("ts_arrestsdetails").setVisible(true);
+            }
         }
         SecurityIncident.onLoad = onLoad;
+        function delaysToOperationOnChange(eContext) {
+            var form = eContext.getFormContext();
+            var delaysToOperations = form.getAttribute("ts_delaystooperation");
+            if (delaysToOperations.getValue() == 717750000 /* Known */) {
+                form.getControl("ts_delaystooperationtime").setVisible(true);
+            }
+        }
+        SecurityIncident.delaysToOperationOnChange = delaysToOperationOnChange;
+        function arrestsOnChange(eContext) {
+            var form = eContext.getFormContext();
+            var arrests = form.getAttribute("ts_arrests");
+            if (arrests.getValue() == 717750000 /* Known */) {
+                form.getControl("ts_arrestscount").setVisible(true);
+                form.getControl("ts_arrestsdetails").setVisible(true);
+            }
+        }
+        SecurityIncident.arrestsOnChange = arrestsOnChange;
     })(SecurityIncident = ROM.SecurityIncident || (ROM.SecurityIncident = {}));
 })(ROM || (ROM = {}));
--- TSIS2.WebResources/src/ts/SecurityIncident.ts
+++ TSIS2.WebResources/src/ts/SecurityIncident.ts
@@ -1,4 +1,35 @@
 namespace ROM.SecurityIncident {
     export function onLoad(eContext: Xrm.ExecutionContext<any, any>): void {
+        const form = <Form.ts_securityincident.Main.Information>eContext.getFormContext();
+
+        if(form.getAttribute("ts_delaystooperation").getValue() == ts_delaystooperation.Known){
+            form.getControl("ts_delaystooperationtime").setVisible(true);
+        }
+
+        if(form.getAttribute("ts_arrests").getValue() == ts_arrestsknownorunknown.Known){
+            form.getControl("ts_arrestscount").setVisible(true);
+            form.getControl("ts_arrestsdetails").setVisible(true);
+        }
+    }
+
+    export function delaysToOperationOnChange(eContext: Xrm.ExecutionContext<any, any>): void {
+        const form = <Form.ts_securityincident.Main.Information>eContext.getFormContext();
+
+        const delaysToOperations = form.getAttribute("ts_delaystooperation");
+
+        if (delaysToOperations.getValue() == ts_delaystooperation.Known) {
+            form.getControl("ts_delaystooperationtime").setVisible(true);
+        }
+    }
+
+    export function arrestsOnChange(eContext: Xrm.ExecutionContext<any, any>): void {
+        const form = <Form.ts_securityincident.Main.Information>eContext.getFormContext();
+
+        const arrests = form.getAttribute("ts_arrests");
+
+        if (arrests.getValue() == ts_arrestsknownorunknown.Known) {
+            form.getControl("ts_arrestscount").setVisible(true);
+            form.getControl("ts_arrestsdetails").setVisible(true);
+        }
     }
 }
