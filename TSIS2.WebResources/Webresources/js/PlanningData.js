3d6f6435c24abb1f1185726bbd7099acde3a32ac
DBurneyTC <Derek.Burney@tc.gc.ca>
2022-12-05 11:08:20 -0500

    Lock fields if they have values on load

--- TSIS2.WebResources/Webresources/js/PlanningData.js
+++ TSIS2.WebResources/Webresources/js/PlanningData.js
@@ -8,16 +8,35 @@ var ROM;
             if (formContext.ui.getFormType() == 2) { //Update type. The form has already been saved for the first time
                 formContext.getControl("ts_fiscalyear").setDisabled(true);
                 formContext.getControl("ts_name").setDisabled(true);
-                formContext.getControl("ts_operationactivity").setDisabled(true);
                 formContext.getControl("ts_teamplanningdata").setDisabled(true);
-                formContext.getControl("ts_target").setDisabled(true);
-                formContext.getControl("ts_dueq1").setDisabled(true);
-                formContext.getControl("ts_dueq2").setDisabled(true);
-                formContext.getControl("ts_dueq3").setDisabled(true);
-                formContext.getControl("ts_dueq4").setDisabled(true);
+                if (formContext.getAttribute("ts_dueq1").getValue != null) {
+                    formContext.getControl("ts_dueq1").setDisabled(true);
+                }
+                if (formContext.getAttribute("ts_dueq2").getValue != null) {
+                    formContext.getControl("ts_dueq2").setDisabled(true);
+                }
+                if (formContext.getAttribute("ts_dueq3").getValue != null) {
+                    formContext.getControl("ts_dueq3").setDisabled(true);
+                }
+                if (formContext.getAttribute("ts_dueq4").getValue != null) {
+                    formContext.getControl("ts_dueq4").setDisabled(true);
+                }
+                if (formContext.getAttribute("ts_operationactivity").getValue() != null) {
+                    formContext.getControl("ts_operationactivity").setDisabled(true);
+                }
+                if (formContext.getAttribute("ts_target").getValue() != null) {
+                    formContext.getControl("ts_target").setDisabled(true);
+                }
+            }
+            if (formContext.getAttribute("ts_generationlog").getValue() == null) {
+                formContext.getControl("ts_generationlog").setVisible(false);
             }
         }
         PlanningData.onLoad = onLoad;
+        function onSave(eContext) {
+            var formContext = eContext.getFormContext();
+        }
+        PlanningData.onSave = onSave;
         function plannedOnChange(eContext) {
             setNullQuarterValueToZero(eContext);
         }
--- TSIS2.WebResources/src/ts/PlanningData.ts
+++ TSIS2.WebResources/src/ts/PlanningData.ts
@@ -4,14 +4,33 @@
         if (formContext.ui.getFormType() == 2) { //Update type. The form has already been saved for the first time
             formContext.getControl("ts_fiscalyear").setDisabled(true);
             formContext.getControl("ts_name").setDisabled(true);
-            formContext.getControl("ts_operationactivity").setDisabled(true);
             formContext.getControl("ts_teamplanningdata").setDisabled(true);
-            formContext.getControl("ts_target").setDisabled(true);
-            formContext.getControl("ts_dueq1").setDisabled(true);
-            formContext.getControl("ts_dueq2").setDisabled(true);
-            formContext.getControl("ts_dueq3").setDisabled(true);
-            formContext.getControl("ts_dueq4").setDisabled(true);
+            if (formContext.getAttribute("ts_dueq1").getValue != null) {
+                formContext.getControl("ts_dueq1").setDisabled(true);
+            }
+            if (formContext.getAttribute("ts_dueq2").getValue != null) {
+                formContext.getControl("ts_dueq2").setDisabled(true);
+            }
+            if (formContext.getAttribute("ts_dueq3").getValue != null) {
+                formContext.getControl("ts_dueq3").setDisabled(true);
+            }
+            if (formContext.getAttribute("ts_dueq4").getValue != null) {
+                formContext.getControl("ts_dueq4").setDisabled(true);
+            }
+            if (formContext.getAttribute("ts_operationactivity").getValue() != null) {
+                formContext.getControl("ts_operationactivity").setDisabled(true);
+            }
+            if (formContext.getAttribute("ts_target").getValue() != null) {
+                formContext.getControl("ts_target").setDisabled(true);
+            }
         }
+        if (formContext.getAttribute("ts_generationlog").getValue() == null) {
+            formContext.getControl("ts_generationlog").setVisible(false);
+        }
+    }
+
+    export function onSave(eContext: Xrm.ExecutionContext<any, any>): void {
+        const formContext = <Form.ts_planningdata.Main.Information>eContext.getFormContext();
     }
 
     export function plannedOnChange(eContext: Xrm.ExecutionContext<any, any>): void {
