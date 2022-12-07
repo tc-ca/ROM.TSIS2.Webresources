c1394c8d03e7b2bf20194d0c61f657269ec15bca
DBurneyTC <Derek.Burney@tc.gc.ca>
2022-12-07 11:02:45 -0500

    Localized alert text. Operation Activity Ribbon buttons

---
+++ TSIS2.WebResources/Webresources/js/OperationActivityRibbon.js
@@ -0,0 +1,25 @@
+﻿async function setSelectedNonOperational(operationActivityGuids, selectedControl) {
+    let data =
+    {
+        "ts_operationalstatus": 717750001,
+    }
+    Xrm.Utility.showProgressIndicator();
+    for (let operationActivityGuid of operationActivityGuids) {
+        await Xrm.WebApi.updateRecord("ts_operationactivity", operationActivityGuid, data);
+    }
+    setTimeout(selectedControl.refresh(), 1000);
+    Xrm.Utility.closeProgressIndicator();
+}
+
+async function setSelectedOperational(operationActivityGuids, selectedControl) {
+    let data =
+    {
+        "ts_operationalstatus": 717750000,
+    }
+    Xrm.Utility.showProgressIndicator();
+    for (let operationActivityGuid of operationActivityGuids) {
+        await Xrm.WebApi.updateRecord("ts_operationactivity", operationActivityGuid, data);
+    }
+    setTimeout(selectedControl.refresh(), 1000);
+    Xrm.Utility.closeProgressIndicator();
+}
--- TSIS2.WebResources/Webresources/js/planningDataRibbon.js
+++ TSIS2.WebResources/Webresources/js/planningDataRibbon.js
@@ -1,4 +1,13 @@
-﻿function setSelectedNonOperational(planningDataGuids, primaryControl) {
+﻿const lang = parent.Xrm.Utility.getGlobalContext().userSettings.languageId;
+let confirmTitle = "Set to Non-Operational"
+let confirmText = "The Operation Activities related to the selected records will become Non-Operational. Do you wish to proceed?"
+
+if (lang == 1036) {
+    confirmTitle = "Passer à non opérationnel"
+    confirmText = "Les activités opérationnelles liées aux enregistrements sélectionnés deviendront non opérationnelles. Voulez-vous continuer?"
+}
+
+function setSelectedNonOperational(planningDataGuids, primaryControl) {
     var confirmStrings = { text: "The Operation Activities related to the selected records will become Non-Operational. Do you wish to proceed?", title: "Set to Non-Operational", confirmButtonLabel: "Yes", cancelButtonLabel: "Cancel" };
     var confirmOptions = { height: 200, width: 450 };
     Xrm.Navigation.openConfirmDialog(confirmStrings, confirmOptions).then(
