2e5130ff806ed7b621948e79ba4737a7dc9e7cd1
DBurneyTC <Derek.Burney@tc.gc.ca>
2023-01-18 16:50:14 -0500

    roleprivileges fetchxml

--- TSIS2.WebResources/Webresources/js/RoleAccess.js
+++ TSIS2.WebResources/Webresources/js/RoleAccess.js
@@ -1,3 +1,23 @@
-﻿"use strict";
+﻿
 function test() {
+    let fetchXml = [
+        "<fetch>",
+        "  <entity name='roleprivileges'>",
+        "    <attribute name='privilegedepthmask'/>",
+        "    <filter>",
+        "      <condition attribute='roleid' operator='eq' value='ed37675e-f72c-eb11-a813-000d3af3a7a7'/>",
+        "    </filter>",
+        "    <link-entity name='privilege' from='privilegeid' to='privilegeid' alias='priv'>",
+        "      <attribute name='name'/>",
+        "      <filter>",
+        "        <condition attribute='name' operator='ends-with' value='msdyn_workorder'/>",
+        "      </filter>",
+        "    </link-entity>",
+        "  </entity>",
+        "</fetch>"
+    ].join("");
+    fetchXml = "?fetchXml=" + encodeURIComponent(fetchXml);
+    parent.Xrm.WebApi.retrieveMultipleRecords("roleprivileges", fetchXml).then(function (result) {
+        console.log(result.entities);
+    });
 }
