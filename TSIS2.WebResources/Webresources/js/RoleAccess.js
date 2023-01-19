2045eefea76c420d928363f9ff9cd0b6241c2242
DBurneyTC <Derek.Burney@tc.gc.ca>
2023-01-19 15:07:46 -0500

    Create multiple tables using listed entities

--- TSIS2.WebResources/Webresources/js/RoleAccess.js
+++ TSIS2.WebResources/Webresources/js/RoleAccess.js
@@ -17,128 +17,140 @@ async function buildRoleAccessTables() {
         { name: "ROM - Business Admin", id: "779105f0-8d3a-eb11-a813-000d3af3fc19" },
     ]
 
+    const powerAppsEntities = [
+        { plainTextName: "Work Order", logicalname: "msdyn_workorder" },
+        { plainTextName: "Case", logicalname: "incident" },
+        { plainTextName: "Operation", logicalname: "ovs_operation" },
+        { plainTextName: "Site", logicalname: "msdyn_functionallocation" },
+        { plainTextName: "Operation Type", logicalname: "ovs_operationtype" },
+        { plainTextName: "Stakeholder", logicalname: "account" },
+    ]
+
     const roleAccessTablesDiv = document.getElementById("roleAccessTables");
-    const workOrderTable = document.createElement("table");
-    roleAccessTablesDiv.appendChild(workOrderTable);
 
-    const workOrderTableHeader = workOrderTable.createTHead();
-    const workOrderTableHeaderTitleRow = workOrderTableHeader.insertRow();
-    const workOrderTableHeaderTitleHeader = document.createElement("th");
-    workOrderTableHeaderTitleHeader.colSpan = 9
-    workOrderTableHeaderTitleHeader.innerHTML = "msdyn_workorder";
-    workOrderTableHeaderTitleRow.appendChild(workOrderTableHeaderTitleHeader)
+    for (let powerAppsEntity of powerAppsEntities) {
+        const sercurityRoleAccessTable = document.createElement("table");
+        roleAccessTablesDiv.appendChild(sercurityRoleAccessTable);
 
-    const workOrderTableHeaderRow = workOrderTableHeader.insertRow();
-    const workOrderTableRoleNameHeader = document.createElement("th");
-    const workOrderTableCreateHeader = document.createElement("th");
-    const workOrderTableReadHeader = document.createElement("th");
-    const workOrderTableWriteHeader = document.createElement("th");
-    const workOrderTableDeleteHeader = document.createElement("th");
-    const workOrderTableAppendHeader = document.createElement("th");
-    const workOrderTableAppendToHeader = document.createElement("th");
-    const workOrderTableAssignHeader = document.createElement("th");
-    const workOrderTableShareHeader = document.createElement("th");
+        const sercurityRoleAccessTableHeader = sercurityRoleAccessTable.createTHead();
+        const sercurityRoleAccessTableHeaderTitleRow = sercurityRoleAccessTableHeader.insertRow();
+        const sercurityRoleAccessTableHeaderTitleHeader = document.createElement("th");
+        sercurityRoleAccessTableHeaderTitleHeader.colSpan = 9
+        sercurityRoleAccessTableHeaderTitleHeader.innerHTML = powerAppsEntity.plainTextName;
+        sercurityRoleAccessTableHeaderTitleRow.appendChild(sercurityRoleAccessTableHeaderTitleHeader)
 
-    workOrderTableRoleNameHeader.innerHTML = "Security Role";
-    workOrderTableCreateHeader.innerHTML = "Create";
-    workOrderTableReadHeader.innerHTML = "Read";
-    workOrderTableWriteHeader.innerHTML = "Write";
-    workOrderTableDeleteHeader.innerHTML = "Delete";
-    workOrderTableAppendHeader.innerHTML = "Append";
-    workOrderTableAppendToHeader.innerHTML = "Append To";
-    workOrderTableAssignHeader.innerHTML = "Assign";
-    workOrderTableShareHeader.innerHTML = "Share";
+        const sercurityRoleAccessTableHeaderRow = sercurityRoleAccessTableHeader.insertRow();
+        const sercurityRoleAccessTableRoleNameHeader = document.createElement("th");
+        const sercurityRoleAccessTableCreateHeader = document.createElement("th");
+        const sercurityRoleAccessTableReadHeader = document.createElement("th");
+        const sercurityRoleAccessTableWriteHeader = document.createElement("th");
+        const sercurityRoleAccessTableDeleteHeader = document.createElement("th");
+        const sercurityRoleAccessTableAppendHeader = document.createElement("th");
+        const sercurityRoleAccessTableAppendToHeader = document.createElement("th");
+        const sercurityRoleAccessTableAssignHeader = document.createElement("th");
+        const sercurityRoleAccessTableShareHeader = document.createElement("th");
 
-    workOrderTableHeaderRow.appendChild(workOrderTableRoleNameHeader);
-    workOrderTableHeaderRow.appendChild(workOrderTableCreateHeader);
-    workOrderTableHeaderRow.appendChild(workOrderTableReadHeader);
-    workOrderTableHeaderRow.appendChild(workOrderTableWriteHeader);
-    workOrderTableHeaderRow.appendChild(workOrderTableDeleteHeader);
-    workOrderTableHeaderRow.appendChild(workOrderTableAppendHeader);
-    workOrderTableHeaderRow.appendChild(workOrderTableAppendToHeader);
-    workOrderTableHeaderRow.appendChild(workOrderTableAssignHeader);
-    workOrderTableHeaderRow.appendChild(workOrderTableShareHeader);
+        sercurityRoleAccessTableRoleNameHeader.innerHTML = "Security Role";
+        sercurityRoleAccessTableCreateHeader.innerHTML = "Create";
+        sercurityRoleAccessTableReadHeader.innerHTML = "Read";
+        sercurityRoleAccessTableWriteHeader.innerHTML = "Write";
+        sercurityRoleAccessTableDeleteHeader.innerHTML = "Delete";
+        sercurityRoleAccessTableAppendHeader.innerHTML = "Append";
+        sercurityRoleAccessTableAppendToHeader.innerHTML = "Append To";
+        sercurityRoleAccessTableAssignHeader.innerHTML = "Assign";
+        sercurityRoleAccessTableShareHeader.innerHTML = "Share";
 
-    for (let securityRole of securityRoles) {
-        let fetchXml = [
-            "<fetch>",
-            "  <entity name='roleprivileges'>",
-            "    <attribute name='privilegedepthmask'/>",
-            "    <filter>",
-            "      <condition attribute='roleid' operator='eq' value='" + securityRole.id + "'/>",
-            "    </filter>",
-            "    <link-entity name='privilege' from='privilegeid' to='privilegeid' alias='priv'>",
-            "      <attribute name='name'/>",
-            "      <filter>",
-            "        <condition attribute='name' operator='ends-with' value='msdyn_workorder'/>",
-            "      </filter>",
-            "    </link-entity>",
-            "  </entity>",
-            "</fetch>"
-        ].join("");
-        fetchXml = "?fetchXml=" + encodeURIComponent(fetchXml);
-        const workOrderPrivileges = await parent.Xrm.WebApi.retrieveMultipleRecords("roleprivileges", fetchXml).then(function (result) { return result.entities });
-        
-        const workOrderPrivilegesData = {
-            Create: 0,
-            Read: 0,
-            Write: 0,
-            Delete: 0,
-            Append: 0,
-            AppendTo: 0,
-            Assign: 0,
-            Share: 0,
-        }
-        for (let privilege of workOrderPrivileges) {
-            switch (privilege["priv.name"]) {
-                case "prvCreatemsdyn_workorder":
-                    workOrderPrivilegesData.Create = privilege.privilegedepthmask
-                    break;
-                case "prvReadmsdyn_workorder":
-                    workOrderPrivilegesData.Read = privilege.privilegedepthmask
-                    break;
-                case "prvWritemsdyn_workorder":
-                    workOrderPrivilegesData.Write = privilege.privilegedepthmask
-                    break;
-                case "prvDeletemsdyn_workorder":
-                    workOrderPrivilegesData.Delete = privilege.privilegedepthmask
-                    break;
-                case "prvAppendmsdyn_workorder":
-                    workOrderPrivilegesData.Append = privilege.privilegedepthmask
-                    break;
-                case "prvAppendTomsdyn_workorder":
-                    workOrderPrivilegesData.AppendTo = privilege.privilegedepthmask
-                    break;
-                case "prvAssignmsdyn_workorder":
-                    workOrderPrivilegesData.Assign = privilege.privilegedepthmask
-                    break;
-                case "prvSharemsdyn_workorder":
-                    workOrderPrivilegesData.Share = privilege.privilegedepthmask
-                    break;
+        sercurityRoleAccessTableHeaderRow.appendChild(sercurityRoleAccessTableRoleNameHeader);
+        sercurityRoleAccessTableHeaderRow.appendChild(sercurityRoleAccessTableCreateHeader);
+        sercurityRoleAccessTableHeaderRow.appendChild(sercurityRoleAccessTableReadHeader);
+        sercurityRoleAccessTableHeaderRow.appendChild(sercurityRoleAccessTableWriteHeader);
+        sercurityRoleAccessTableHeaderRow.appendChild(sercurityRoleAccessTableDeleteHeader);
+        sercurityRoleAccessTableHeaderRow.appendChild(sercurityRoleAccessTableAppendHeader);
+        sercurityRoleAccessTableHeaderRow.appendChild(sercurityRoleAccessTableAppendToHeader);
+        sercurityRoleAccessTableHeaderRow.appendChild(sercurityRoleAccessTableAssignHeader);
+        sercurityRoleAccessTableHeaderRow.appendChild(sercurityRoleAccessTableShareHeader);
 
+        for (let securityRole of securityRoles) {
+            let fetchXml = [
+                "<fetch>",
+                "  <entity name='roleprivileges'>",
+                "    <attribute name='privilegedepthmask'/>",
+                "    <filter>",
+                "      <condition attribute='roleid' operator='eq' value='" + securityRole.id + "'/>",
+                "    </filter>",
+                "    <link-entity name='privilege' from='privilegeid' to='privilegeid' alias='priv'>",
+                "      <attribute name='name'/>",
+                "      <filter>",
+                "        <condition attribute='name' operator='ends-with' value='" + powerAppsEntity.logicalname + "'/>",
+                "      </filter>",
+                "    </link-entity>",
+                "  </entity>",
+                "</fetch>"
+            ].join("");
+            fetchXml = "?fetchXml=" + encodeURIComponent(fetchXml);
+            const workOrderPrivileges = await parent.Xrm.WebApi.retrieveMultipleRecords("roleprivileges", fetchXml).then(function (result) { return result.entities });
+
+            const workOrderPrivilegesData = {
+                Create: 0,
+                Read: 0,
+                Write: 0,
+                Delete: 0,
+                Append: 0,
+                AppendTo: 0,
+                Assign: 0,
+                Share: 0,
             }
+            for (let privilege of workOrderPrivileges) {
+                switch (privilege["priv.name"].toLowerCase()) {
+                    case "prvcreate" + powerAppsEntity.logicalname:
+                        workOrderPrivilegesData.Create = privilege.privilegedepthmask
+                        break;
+                    case "prvread" + powerAppsEntity.logicalname:
+                        workOrderPrivilegesData.Read = privilege.privilegedepthmask
+                        break;
+                    case "prvwrite" + powerAppsEntity.logicalname:
+                        workOrderPrivilegesData.Write = privilege.privilegedepthmask
+                        break;
+                    case "prvdelete" + powerAppsEntity.logicalname:
+                        workOrderPrivilegesData.Delete = privilege.privilegedepthmask
+                        break;
+                    case "prvappend" + powerAppsEntity.logicalname:
+                        workOrderPrivilegesData.Append = privilege.privilegedepthmask
+                        break;
+                    case "prvappendTo" + powerAppsEntity.logicalname:
+                        workOrderPrivilegesData.AppendTo = privilege.privilegedepthmask
+                        break;
+                    case "prvassign" + powerAppsEntity.logicalname:
+                        workOrderPrivilegesData.Assign = privilege.privilegedepthmask
+                        break;
+                    case "prvshare" + powerAppsEntity.logicalname:
+                        workOrderPrivilegesData.Share = privilege.privilegedepthmask
+                        break;
+
+                }
+            }
+
+            const sercurityRoleAccessTableDataBody = sercurityRoleAccessTable.createTBody();
+            const sercurityRoleAccessTableDataRow = sercurityRoleAccessTableDataBody.insertRow();
+            const sercurityRoleAccessTableRoleNameData = sercurityRoleAccessTableDataRow.insertCell();
+            const sercurityRoleAccessTableCreateData = sercurityRoleAccessTableDataRow.insertCell();
+            const sercurityRoleAccessTableReadData = sercurityRoleAccessTableDataRow.insertCell();
+            const sercurityRoleAccessTableWriteData = sercurityRoleAccessTableDataRow.insertCell();
+            const sercurityRoleAccessTableDeleteData = sercurityRoleAccessTableDataRow.insertCell();
+            const sercurityRoleAccessTableAppendData = sercurityRoleAccessTableDataRow.insertCell();
+            const sercurityRoleAccessTableAppendToData = sercurityRoleAccessTableDataRow.insertCell();
+            const sercurityRoleAccessTableAssignData = sercurityRoleAccessTableDataRow.insertCell();
+            const sercurityRoleAccessTableShareData = sercurityRoleAccessTableDataRow.insertCell();
+
+            sercurityRoleAccessTableRoleNameData.innerHTML = securityRole.name;
+            sercurityRoleAccessTableCreateData.innerHTML = workOrderPrivilegesData.Create;
+            sercurityRoleAccessTableReadData.innerHTML = workOrderPrivilegesData.Read;
+            sercurityRoleAccessTableWriteData.innerHTML = workOrderPrivilegesData.Write;
+            sercurityRoleAccessTableDeleteData.innerHTML = workOrderPrivilegesData.Delete;
+            sercurityRoleAccessTableAppendData.innerHTML = workOrderPrivilegesData.Append;
+            sercurityRoleAccessTableAppendToData.innerHTML = workOrderPrivilegesData.AppendTo;
+            sercurityRoleAccessTableAssignData.innerHTML = workOrderPrivilegesData.Assign;
+            sercurityRoleAccessTableShareData.innerHTML = workOrderPrivilegesData.Share;
         }
-
-        const workOrderTableDataBody = workOrderTable.createTBody();
-        const workOrderTableDataRow = workOrderTableDataBody.insertRow();
-        const workOrderTableRoleNameData = workOrderTableDataRow.insertCell();
-        const workOrderTableCreateData = workOrderTableDataRow.insertCell();
-        const workOrderTableReadData = workOrderTableDataRow.insertCell();
-        const workOrderTableWriteData = workOrderTableDataRow.insertCell();
-        const workOrderTableDeleteData = workOrderTableDataRow.insertCell();
-        const workOrderTableAppendData = workOrderTableDataRow.insertCell();
-        const workOrderTableAppendToData = workOrderTableDataRow.insertCell();
-        const workOrderTableAssignData = workOrderTableDataRow.insertCell();
-        const workOrderTableShareData = workOrderTableDataRow.insertCell();
-
-        workOrderTableRoleNameData.innerHTML = securityRole.name;
-        workOrderTableCreateData.innerHTML = workOrderPrivilegesData.Create;
-        workOrderTableReadData.innerHTML = workOrderPrivilegesData.Read;
-        workOrderTableWriteData.innerHTML = workOrderPrivilegesData.Write;
-        workOrderTableDeleteData.innerHTML = workOrderPrivilegesData.Delete;
-        workOrderTableAppendData.innerHTML = workOrderPrivilegesData.Append;
-        workOrderTableAppendToData.innerHTML = workOrderPrivilegesData.AppendTo;
-        workOrderTableAssignData.innerHTML = workOrderPrivilegesData.Assign;
-        workOrderTableShareData.innerHTML = workOrderPrivilegesData.Share;
     }
 }
