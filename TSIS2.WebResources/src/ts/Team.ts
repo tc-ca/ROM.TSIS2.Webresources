30d5a68b82728936b7f873c9af1d4ebe8661deba
DBurneyTC <Derek.Burney@tc.gc.ca>
2023-01-25 16:16:51 -0500

    Planning Tab visibility

--- TSIS2.WebResources/src/ts/Team.ts
+++ TSIS2.WebResources/src/ts/Team.ts
@@ -1,4 +1,18 @@
 namespace ROM.Team {
+    export function onLoad(eContext: Xrm.ExecutionContext<any, any>) {
+        //Show the Planning Tab only when in the Oversight Planning Module
+        var globalContext = Xrm.Utility.getGlobalContext();
+        globalContext.getCurrentAppName().then(function (appName) {
+            if (appName == "Oversight Planning Module") {
+                const formContext = <Form.team.Main.Team>eContext.getFormContext();
+                const planningTab = formContext.ui.tabs.get("tab_planning");
+                if (planningTab != null) {
+                    planningTab.setVisible(true);
+                }
+            }
+        });
+    }
+
     export function setFieldsDisabled(eContext: Xrm.ExecutionContext<any, any>) {
         const formContext = <Form.ovs_operation.Main.Information>eContext.getFormContext();
         const gridContext = formContext.getControl("activity_type_estimated_duration");
