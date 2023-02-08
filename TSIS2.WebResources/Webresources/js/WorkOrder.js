03a2d72336fb94f5c14b1a891a5d0496c7e9abbc
Elena0925 <59444477+Elena0925@users.noreply.github.com>
2023-02-08 08:58:03 -0500

    Expose Planning Data Details field

--- TSIS2.WebResources/Webresources/js/WorkOrder.js
+++ TSIS2.WebResources/Webresources/js/WorkOrder.js
@@ -1,4 +1,40 @@
 ﻿"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 var ROM;
 (function (ROM) {
@@ -17,6 +53,29 @@ var ROM;
             var regionAttributeValue = regionAttribute.getValue();
             var ownerControl = form.getControl("ownerid");
             var headerOwnerControl = form.getControl("header_ownerid");
+            //Set comment field visible if AvSec
+            var userBusinessUnitName;
+            var userId = Xrm.Utility.getGlobalContext().userSettings.userId;
+            var currentUserBusinessUnitFetchXML = [
+                "<fetch top='50'>",
+                "  <entity name='businessunit'>",
+                "    <attribute name='name' />",
+                "    <attribute name='businessunitid' />",
+                "    <link-entity name='systemuser' from='businessunitid' to='businessunitid' link-type='inner' alias='ab'>>",
+                "      <filter>",
+                "        <condition attribute='systemuserid' operator='eq' value='", userId, "'/>",
+                "      </filter>",
+                "    </link-entity>",
+                "  </entity>",
+                "</fetch>",
+            ].join("");
+            currentUserBusinessUnitFetchXML = "?fetchXml=" + encodeURIComponent(currentUserBusinessUnitFetchXML);
+            Xrm.WebApi.retrieveMultipleRecords("businessunit", currentUserBusinessUnitFetchXML).then(function (businessunit) {
+                userBusinessUnitName = businessunit.entities[0].name;
+                if (userBusinessUnitName.startsWith("Aviation")) {
+                    form.getControl("ts_details").setVisible(true);
+                }
+            });
             //Keep track of the current system status, to be used when cancelling a status change.
             currentSystemStatus = form.getAttribute("msdyn_systemstatus").getValue();
             currentStatus = form.getAttribute("ts_state").getValue();
@@ -1443,6 +1502,35 @@ var ROM;
                     Xrm.Navigation.openAlertDialog({ text: error.message });
                 });
             }
+        }
+        function isAvSecBusinessUnit() {
+            return __awaiter(this, void 0, void 0, function () {
+                var userId, currentUserBusinessUnitFetchXML, userBusinessUnitName;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            userId = Xrm.Utility.getGlobalContext().userSettings.userId;
+                            currentUserBusinessUnitFetchXML = [
+                                "<fetch top='50'>",
+                                "  <entity name='businessunit'>",
+                                "    <attribute name='name' />",
+                                "    <attribute name='businessunitid' />",
+                                "    <link-entity name='systemuser' from='businessunitid' to='businessunitid' link-type='inner' alias='ab'>>",
+                                "      <filter>",
+                                "        <condition attribute='systemuserid' operator='eq' value='", userId, "'/>",
+                                "      </filter>",
+                                "    </link-entity>",
+                                "  </entity>",
+                                "</fetch>",
+                            ].join("");
+                            currentUserBusinessUnitFetchXML = "?fetchXml=" + encodeURIComponent(currentUserBusinessUnitFetchXML);
+                            return [4 /*yield*/, Xrm.WebApi.retrieveMultipleRecords("businessunit", currentUserBusinessUnitFetchXML)];
+                        case 1:
+                            userBusinessUnitName = _a.sent();
+                            return [2 /*return*/, userBusinessUnitName.entities[0].name.startsWith("Aviation")];
+                    }
+                });
+            });
         }
     })(WorkOrder = ROM.WorkOrder || (ROM.WorkOrder = {}));
 })(ROM || (ROM = {}));
