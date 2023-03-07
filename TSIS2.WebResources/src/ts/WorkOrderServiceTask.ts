2b73acabc4c110b10d5d5c34dbabf81833d2d663
Elena0925 <59444477+Elena0925@users.noreply.github.com>
2023-03-07 10:13:54 -0500

    Change the way Accountable Operations are created for Findings in a questionnaire

--- TSIS2.WebResources/src/ts/WorkOrderServiceTask.ts
+++ TSIS2.WebResources/src/ts/WorkOrderServiceTask.ts
@@ -645,37 +645,39 @@ namespace ROM.WorkOrderServiceTask {
         //Retrieve the operation in the ovs_operationid field of the parent work order
         let operationPromise1 = Xrm.WebApi.retrieveMultipleRecords("msdyn_workorder", parentWorkOrderOperationFetchXml);
 
+        var workOrderServiceTaskId = eContext.getFormContext().data.entity.getId();
 
-        var parentWorkOrderRelatedOperationFetchXml = [
-            "<fetch top='50'>",
-            "  <entity name='ovs_operation'>",
-            "    <attribute name='ts_stakeholder' />",
-            "    <attribute name='ovs_operationid' />",
-            "    <attribute name='ovs_name' />",
-            "    <link-entity name='ts_msdyn_workorder_ovs_operation' from='ovs_operationid' to='ovs_operationid' intersect='true'>",
-            "      <filter>",
-            "        <condition attribute='msdyn_workorderid' operator='eq' value='", workOrderId, "'/>",
-            "      </filter>",
-            "    </link-entity>",
+        var AOCOperationFetchXml = [
+            "<fetch>",
+            "<entity name='ovs_operation'>",
+            "<attribute name='ovs_name'/>",
+            "<attribute name='ovs_operationtypeid'/>",
+            "<attribute name='ovs_operationid'/>",
+            "<order attribute='ovs_name'/>",
+            "<link-entity name = 'msdyn_workorderservicetask' from='ts_aocoperation' to='ovs_operationid'>",
+            "<filter type='and'>",
+            "<condition attribute='msdyn_workorderservicetaskid' operator='eq' value='", workOrderServiceTaskId, "'/>",
+            "</filter>",
+            "</link-entity>",
+            "<link-entity name='ovs_operationtype' from='ovs_operationtypeid' to='ovs_operationtypeid'>",
+            "<attribute name='ts_regulated'/>",
+            "<attribute name='ovs_operationtypeid'/>",
+            "<attribute name='ovs_operationtypenameenglish'/>",
+            "<attribute name='ovs_operationtypenamefrench'/>",
+            "</link-entity>",
+            "<link-entity name='msdyn_functionallocation' from='msdyn_functionallocationid' to='ts_site'>",
+            "<attribute name='ts_functionallocationnamefrench'/>",
+            "<attribute name='ts_functionallocationnameenglish'/>",
+            "</link-entity>",
             "    <link-entity name='account' from='accountid' to='ts_stakeholder'>",
             "      <attribute name='name' />",
             "    </link-entity>",
-            "    <link-entity name='ovs_operationtype' from='ovs_operationtypeid' to='ovs_operationtypeid'>",
-            "      <attribute name='ts_regulated' />",
-            "      <attribute name='ovs_operationtypeid' /> ",
-            "      <attribute name='ovs_operationtypenameenglish' />",
-            "      <attribute name='ovs_operationtypenamefrench' />",
-            "    </link-entity>",
-            "    <link-entity name='msdyn_functionallocation' from='msdyn_functionallocationid' to='ts_site'>",
-            "      <attribute name='ts_functionallocationnamefrench' />",
-            "      <attribute name='ts_functionallocationnameenglish' />",
-            "    </link-entity>",
-            "  </entity>",
-            "</fetch>",
+            "</entity>",
+            "</fetch>"
         ].join("");
-        parentWorkOrderRelatedOperationFetchXml = "?fetchXml=" + encodeURIComponent(parentWorkOrderRelatedOperationFetchXml);
-        //Retrieve operations associated to the parent Work Order
-        let operationPromise2 = Xrm.WebApi.retrieveMultipleRecords("ovs_operation", parentWorkOrderRelatedOperationFetchXml);
+        AOCOperationFetchXml = "?fetchXml=" + encodeURIComponent(AOCOperationFetchXml);
+        //Retrieve AOC operation
+        let operationPromise2 = Xrm.WebApi.retrieveMultipleRecords("ovs_operation", AOCOperationFetchXml);
 
         var activityTypeOperationTypesFetchXML = [
             "<fetch top='50'>",
@@ -711,26 +713,26 @@ namespace ROM.WorkOrderServiceTask {
                     isRegulated: workOrderOperation["ovs_operationtype2.ts_regulated"]
                 });
             }
-            
-            if (workOrderOperation["_msdyn_workordertype_value"] != null)
-            {
-                if (workOrderOperation["_msdyn_workordertype_value"].toUpperCase() == "B1EE680A-7CF7-EA11-A815-000D3AF3A7A7")
-                {
+
+            if (workOrderOperation["_msdyn_workordertype_value"] != null) {
+                if (workOrderOperation["_msdyn_workordertype_value"].toUpperCase() == "B1EE680A-7CF7-EA11-A815-000D3AF3A7A7") {
                     isInspectionType = true;
                 }
             }
-            //Add the operationid, name, operationTypeId, and regulated boolean of the work order's N:N operations to the operations array
+            //Add the operationid, name, operationTypeId, and regulated boolean of the AOC operation
             operationRetrievalPromises[1].entities.forEach(function (operation) {
-                let stakeholderName = operation["account2.name"];
-                let operationTypeName = (lang == 1036) ? operation["ovs_operationtype3.ovs_operationtypenamefrench"] : operation["ovs_operationtype3.ovs_operationtypenameenglish"];
-                let siteName = (lang == 1036) ? operation["msdyn_functionallocation4.ts_functionallocationnamefrench"] : operation["msdyn_functionallocation4.ts_functionallocationnameenglish"];
-                if (operation.ovs_operationid != null && operation["account2.name"] != null && operation["ovs_operationtype3.ts_regulated"] != null) {
-                    operations.push({
-                        id: operation["ovs_operationid"],
-                        name: stakeholderName + " | " + operationTypeName + " | " + siteName,
-                        operationTypeId: operation["ovs_operationtype3.ovs_operationtypeid"],
-                        isRegulated: operation["ovs_operationtype3.ts_regulated"]
-                    });
+                let stakeholderName = operation["account4.name"];
+                let operationTypeName = (lang == 1036) ? operation["ovs_operationtype2.ovs_operationtypenamefrench"] : operation["ovs_operationtype2.ovs_operationtypenameenglish"];
+                let siteName = (lang == 1036) ? operation["msdyn_functionallocation3.ts_functionallocationnamefrench"] : operation["msdyn_functionallocation3.ts_functionallocationnameenglish"];
+                if (operation.ovs_operationid != null && operation["account4.name"] != null && operation["ovs_operationtype2.ts_regulated"] != null) {
+                    if ((operations[0]["id"] != operation.ovs_operationid)) {
+                        operations.push({
+                            id: operation["ovs_operationid"],
+                            name: stakeholderName + " | " + operationTypeName + " | " + siteName,
+                            operationTypeId: operation["ovs_operationtype2.ovs_operationtypeid"],
+                            isRegulated: operation["ovs_operationtype2.ts_regulated"]
+                        });
+                    }
                 }
             });
 
