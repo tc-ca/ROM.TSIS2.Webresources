5c9c79f714b1d44564d9c4213ef06e0aaac66921
H <hamza.etth@gmail.com>
2023-06-29 15:20:40 -0400

    Visibility Rules for the Action entity (enforcement)

--- TSIS2.WebResources/Webresources/js/Action.js
+++ TSIS2.WebResources/Webresources/js/Action.js
@@ -3,14 +3,82 @@ var ROM;
 (function (ROM) {
     var Action;
     (function (Action) {
+        var allDeliveryMethodOptions = [
+            { text: "Verbal", value: 741130000 /* ts_deliverymethod.Verbal */ },
+            { text: "In Person", value: 741130006 /* ts_deliverymethod.InPerson */ },
+            { text: "Telephone", value: 741130007 /* ts_deliverymethod.Telephone */ },
+            { text: "Email", value: 741130001 /* ts_deliverymethod.Email */ },
+            { text: "SSCIMS", value: 741130002 /* ts_deliverymethod.SSCIMS */ },
+            { text: "Letter - Hand delivered", value: 741130003 /* ts_deliverymethod.LetterHandDelivered */ },
+            { text: "Letter - Mail", value: 741130004 /* ts_deliverymethod.LetterMail */ },
+            { text: "Letter - Registered Mail", value: 741130005 /* ts_deliverymethod.LetterRegisteredMail */ }
+        ];
         function onLoad(eContext) {
             var form = eContext.getFormContext();
             if (form.ui.getFormType() != 0 && form.ui.getFormType() != 1 && form.ui.getFormType() != 6) {
                 setRelatedFindingsFetchXML(form);
             }
+            else if (form.ui.getFormType() != 2) {
+                actionCategoryOnChange(eContext);
+            }
             actionStatusOnChange(eContext);
         }
         Action.onLoad = onLoad;
+        function setOptions(attribute, options) {
+            attribute.clearOptions();
+            options.forEach(function (option) { return attribute.addOption(option); });
+        }
+        function resetFieldsVisibility(form) {
+            form.getControl("ts_stakeholder").setVisible(true);
+            form.getControl("ts_contact").setVisible(true);
+            form.getControl("ts_deliverymethod").setVisible(true);
+            form.getControl("ts_location").setVisible(true);
+        }
+        function actionCategoryOnChange(eContext) {
+            var form = eContext.getFormContext();
+            var actionCategoryAttributeValue = form.getAttribute("ts_actioncategory").getValue();
+            var actionTypeAttributeValue = form.getAttribute("ts_actiontype").getValue();
+            var deliveryMethodAttribute = form.getControl("ts_deliverymethod");
+            if (actionCategoryAttributeValue == 741130002 /* ts_actioncategory.EnforcementAction */) {
+                var filteredOptions = allDeliveryMethodOptions;
+                if (actionTypeAttributeValue == 741130012 /* ts_actiontype.VerbalWarning */) {
+                    filteredOptions = [
+                        { text: "In Person", value: 741130006 /* ts_deliverymethod.InPerson */ },
+                        { text: "Telephone", value: 741130007 /* ts_deliverymethod.Telephone */ },
+                        { text: "Email", value: 741130001 /* ts_deliverymethod.Email */ }
+                    ];
+                    resetFieldsVisibility(form);
+                    form.getControl("ts_location").setVisible(false);
+                }
+                else if (actionTypeAttributeValue == 741130013 /* ts_actiontype.WrittenWarning */) {
+                    filteredOptions = [
+                        { text: "Email", value: 741130001 /* ts_deliverymethod.Email */ },
+                        { text: "Letter - Hand delivered", value: 741130003 /* ts_deliverymethod.LetterHandDelivered */ },
+                        { text: "Letter - Mail", value: 741130004 /* ts_deliverymethod.LetterMail */ },
+                        { text: "Letter - Registered Mail", value: 741130005 /* ts_deliverymethod.LetterRegisteredMail */ },
+                        { text: "SSCIMS", value: 741130002 /* ts_deliverymethod.SSCIMS */ }
+                    ];
+                    resetFieldsVisibility(form);
+                    form.getControl("ts_location").setVisible(false);
+                }
+                else if (actionTypeAttributeValue == 741130017 /* ts_actiontype.RegionalEnforcementUnitREU */) {
+                    form.getControl("ts_stakeholder").setVisible(false);
+                    form.getControl("ts_contact").setVisible(false);
+                    form.getControl("ts_deliverymethod").setVisible(false);
+                    form.getControl("ts_location").setVisible(false);
+                }
+                else {
+                    filteredOptions = allDeliveryMethodOptions;
+                    resetFieldsVisibility(form);
+                }
+                setOptions(deliveryMethodAttribute, filteredOptions);
+            }
+            else {
+                resetFieldsVisibility(form);
+                setOptions(deliveryMethodAttribute, allDeliveryMethodOptions);
+            }
+        }
+        Action.actionCategoryOnChange = actionCategoryOnChange;
         function setRelatedFindingsFetchXML(form) {
             var gridControl = form.getControl("subgrid_related_findings");
             if (gridControl === null) {
@@ -19,7 +87,7 @@ var ROM;
             }
             else {
                 var actionId = form.data.entity.getId();
-                var fetchXml = "<link-entity name=\"ts_actionfinding\" from=\"ts_ovs_finding\" to=\"ovs_findingid\" link-type=\"inner\" alias=\"aa\"><attribute name=\"ts_ovs_finding\"/><filter type=\"and\"><condition attribute=\"ts_ovs_finding\" operator=\"not-null\"/></filter><link-entity name=\"ts_action\" from=\"ts_actionid\" to=\"ts_action\" link-type=\"inner\" alias=\"ab\"><attribute name=\"ts_actionid\"/><filter type=\"and\"><condition attribute=\"ts_actionid\" operator=\"eq\" value=\"" + actionId + "\"/></filter></link-entity></link-entity>";
+                var fetchXml = "<link-entity name=\"ts_actionfinding\" from=\"ts_ovs_finding\" to=\"ovs_findingid\" link-type=\"inner\" alias=\"aa\"><attribute name=\"ts_ovs_finding\"/><filter type=\"and\"><condition attribute=\"ts_ovs_finding\" operator=\"not-null\"/></filter><link-entity name=\"ts_action\" from=\"ts_actionid\" to=\"ts_action\" link-type=\"inner\" alias=\"ab\"><attribute name=\"ts_actionid\"/><filter type=\"and\"><condition attribute=\"ts_actionid\" operator=\"eq\" value=\"".concat(actionId, "\"/></filter></link-entity></link-entity>");
                 ROM.Utils.setSubgridFilterXml(form, "subgrid_related_findings", fetchXml);
             }
         }
@@ -27,7 +95,7 @@ var ROM;
         function actionStatusOnChange(eContext) {
             var form = eContext.getFormContext();
             var actionStatus = form.getAttribute("ts_actionstatus").getValue();
-            if (actionStatus != null && (actionStatus == 741130000 /* Consulted */ || actionStatus == 741130001 /* Convened */)) {
+            if (actionStatus != null && (actionStatus == 741130000 /* ts_actionstatus.Consulted */ || actionStatus == 741130001 /* ts_actionstatus.Convened */)) {
                 form.getControl("ts_deliverymethod").setVisible(false);
                 form.getControl("ts_amtamount").setVisible(false);
                 form.getControl("ts_duedate").setVisible(false);
@@ -37,9 +105,9 @@ var ROM;
                 form.getControl("ts_amtamount").setVisible(true);
                 form.getControl("ts_duedate").setVisible(true);
                 var actionType = form.getAttribute("ts_actiontype").getValue();
-                if (actionType != null && actionType == 741130007 /* AMPPayment */) {
+                if (actionType != null && actionType == 741130007 /* ts_actiontype.AMPPayment */) {
                     form.getControl("ts_amtamount").setVisible(true);
-                    if (actionStatus != null && actionStatus == 741130004 /* Requested */) {
+                    if (actionStatus != null && actionStatus == 741130004 /* ts_actionstatus.Requested */) {
                         form.getControl("ts_duedate").setVisible(true);
                     }
                     else {
@@ -55,6 +123,7 @@ var ROM;
         Action.actionStatusOnChange = actionStatusOnChange;
         function actionTypeOnChange(eContext) {
             actionStatusOnChange(eContext);
+            actionCategoryOnChange(eContext);
         }
         Action.actionTypeOnChange = actionTypeOnChange;
     })(Action = ROM.Action || (ROM.Action = {}));
