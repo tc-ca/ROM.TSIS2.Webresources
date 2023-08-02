fa15d061c03df0f8d3f9ece6b1a88e6c7e159d58
DBurneyTC <Derek.Burney@tc.gc.ca>
2023-08-02 09:56:10 -0400

    fix recalculate logic

--- TSIS2.WebResources/Webresources/js/PlanRibbon.js
+++ TSIS2.WebResources/Webresources/js/PlanRibbon.js
@@ -19,19 +19,21 @@
         "</fetch>"
     ].join("");
     planInspectorHoursFetchXml = "?fetchXml=" + encodeURIComponent(planInspectorHoursFetchXml);
+    let inspectorIds = [];
     let planInspectorHours = await Xrm.WebApi.retrieveMultipleRecords("ts_planinspectorhours", planInspectorHoursFetchXml).then(function success(result) {
         //Convert the result to a format easier to work with
-        let inspectorHours = [];
-        for (let planInspectorHoursRecord of planInspectorHours) {
-            //Set the index of the object in the array as the id of the inspector
+        let inspectorHours = {};
+        for (let planInspectorHoursRecord of result.entities) {
+            //Total hours data for an inspector will be stored as property on the inspectorHours object, with the inspector's id as the property's name
             //Start the remaining hours as the total hours
-            inspectorHours[planInspectorHoursRecord.inspectorhours.ts_inspector] = {
+            inspectorHours[planInspectorHoursRecord["inspectorhours.ts_inspector"]] = {
                 remainingHoursQ1: planInspectorHoursRecord.ts_totalhoursq1,
                 remainingHoursQ2: planInspectorHoursRecord.ts_totalhoursq2,
                 remainingHoursQ3: planInspectorHoursRecord.ts_totalhoursq3,
                 remainingHoursQ4: planInspectorHoursRecord.ts_totalhoursq4,
                 planInspectorHoursId: planInspectorHoursRecord.ts_planinspectorhoursid
             }
+            inspectorIds.push(planInspectorHoursRecord["inspectorhours.ts_inspector"]);
         }
         return inspectorHours;
     });
@@ -54,28 +56,30 @@
         "</fetch>"
     ].join("");
     suggestedInspectionsFetchXml = "?fetchXml=" + encodeURIComponent(suggestedInspectionsFetchXml);
-    let suggestedInspections = await Xrm.WebApi.retrieveMultipleRecords("ts_planinspectorhours", planInspectorHoursFetchXml).then(function success(result) {
+    let suggestedInspections = await Xrm.WebApi.retrieveMultipleRecords("ts_suggestedinspection", suggestedInspectionsFetchXml).then(function success(result) {
         return result.entities;
     });
     //For each suggested inspection, if the assigned inspector has a matching inspector hours object, subtract from the total hours of each quarter 
     for (let suggestedInspection of suggestedInspections) {
-        if (planInspectorHours[suggestedInspection.ts_inspector] != null) {
-            planInspectorHours[suggestedInspection.ts_inspector].remainingHoursQ1 -= suggestedInspection.ts_q1 * suggestedInspection["activitytype.msdyn_estimatedduration"];
-            planInspectorHours[suggestedInspection.ts_inspector].remainingHoursQ2 -= suggestedInspection.ts_q2 * suggestedInspection["activitytype.msdyn_estimatedduration"];
-            planInspectorHours[suggestedInspection.ts_inspector].remainingHoursQ3 -= suggestedInspection.ts_q3 * suggestedInspection["activitytype.msdyn_estimatedduration"];
-            planInspectorHours[suggestedInspection.ts_inspector].remainingHoursQ4 -= suggestedInspection.ts_q4 * suggestedInspection["activitytype.msdyn_estimatedduration"];
+        if (planInspectorHours[suggestedInspection._ts_inspector_value] != null) {
+            planInspectorHours[suggestedInspection._ts_inspector_value].remainingHoursQ1 -= suggestedInspection.ts_q1 * (suggestedInspection["activitytype.msdyn_estimatedduration"] / 60);
+            planInspectorHours[suggestedInspection._ts_inspector_value].remainingHoursQ2 -= suggestedInspection.ts_q2 * (suggestedInspection["activitytype.msdyn_estimatedduration"] / 60);
+            planInspectorHours[suggestedInspection._ts_inspector_value].remainingHoursQ3 -= suggestedInspection.ts_q3 * (suggestedInspection["activitytype.msdyn_estimatedduration"] / 60);
+            planInspectorHours[suggestedInspection._ts_inspector_value].remainingHoursQ4 -= suggestedInspection.ts_q4 * (suggestedInspection["activitytype.msdyn_estimatedduration"] / 60);
         }
     }
 
     //Update each insectorHours with the updated remaining hours
-    for (let planInspectorHoursObject of planInspectorHours) {
-        let data =
-        {
-            "ts_remaininghoursq1": planInspectorHoursObject.remainingHoursQ1,
-            "ts_remaininghoursq2": planInspectorHoursObject.remainingHoursQ2,
-            "ts_remaininghoursq3": planInspectorHoursObject.remainingHoursQ3,
-            "ts_remaininghoursq4": planInspectorHoursObject.remainingHoursQ4
+    for (let inspectorId of inspectorIds) {
+        if (planInspectorHours[inspectorId] != null) {
+            let data =
+            {
+                "ts_remaininghoursq1": planInspectorHours[inspectorId].remainingHoursQ1,
+                "ts_remaininghoursq2": planInspectorHours[inspectorId].remainingHoursQ2,
+                "ts_remaininghoursq3": planInspectorHours[inspectorId].remainingHoursQ3,
+                "ts_remaininghoursq4": planInspectorHours[inspectorId].remainingHoursQ4
+            }
+            Xrm.WebApi.updateRecord("ts_planinspectorhours", planInspectorHours[inspectorId].planInspectorHoursId, data)
         }
-        Xrm.WebApi.updateRecord("ts_planinspectorhours", planInspectorHoursRecord.planInspectorHoursId, data)
     }
 }
