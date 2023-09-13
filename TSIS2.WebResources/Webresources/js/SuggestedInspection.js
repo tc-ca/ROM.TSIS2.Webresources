00d2b83d41a3ee6f6d3c7b3f1db4dd8e76fcc6e1
DBurneyTC <Derek.Burney@tc.gc.ca>
2023-09-13 15:00:52 -0400

    spkl. Suggested inspection onchange and filter functions

--- TSIS2.WebResources/Webresources/js/SuggestedInspection.js
+++ TSIS2.WebResources/Webresources/js/SuggestedInspection.js
@@ -8,20 +8,112 @@ var ROM;
             setOperationTypeFilteredView(form);
         }
         SuggestedInspection.onLoad = onLoad;
+        function operationTypeOnChange(eContext) {
+            var form = eContext.getFormContext();
+            var operationTypeValue = form.getAttribute("ts_operationtype").getValue();
+            if (operationTypeValue == null) {
+                //Clear and lock all dependent fields
+                form.getAttribute("ts_stakeholder").setValue(null);
+                form.getAttribute("ts_site").setValue(null);
+                form.getAttribute("ts_operation").setValue(null);
+                form.getAttribute("ts_activitytype").setValue(null);
+                form.getAttribute("ts_riskthreshold").setValue(null);
+                form.getControl("ts_stakeholder").setDisabled(true);
+                form.getControl("ts_site").setDisabled(true);
+                form.getControl("ts_operation").setDisabled(true);
+                form.getControl("ts_activitytype").setDisabled(true);
+                form.getControl("ts_riskthreshold").setDisabled(true);
+            }
+            else {
+                //Unlock next field
+                form.getControl("ts_stakeholder").setDisabled(false);
+                setStakeholderFilteredView(form);
+            }
+        }
+        SuggestedInspection.operationTypeOnChange = operationTypeOnChange;
+        function stakeholderOnChange(eContext) {
+            var form = eContext.getFormContext();
+            var stakeholderValue = form.getAttribute("ts_stakeholder").getValue();
+            if (stakeholderValue == null) {
+                //Clear and lock all dependent fields
+                form.getAttribute("ts_site").setValue(null);
+                form.getAttribute("ts_operation").setValue(null);
+                form.getAttribute("ts_activitytype").setValue(null);
+                form.getAttribute("ts_riskthreshold").setValue(null);
+                form.getControl("ts_site").setDisabled(true);
+                form.getControl("ts_operation").setDisabled(true);
+                form.getControl("ts_activitytype").setDisabled(true);
+                form.getControl("ts_riskthreshold").setDisabled(true);
+            }
+            else {
+                //Unlock next field
+                form.getControl("ts_site").setDisabled(false);
+                setSiteFilteredView(form);
+            }
+        }
+        SuggestedInspection.stakeholderOnChange = stakeholderOnChange;
+        function siteOnChange(eContext) {
+            var form = eContext.getFormContext();
+            var siteValue = form.getAttribute("ts_site").getValue();
+            if (siteValue == null) {
+                //Clear and lock all dependent fields
+                form.getAttribute("ts_operation").setValue(null);
+                form.getAttribute("ts_activitytype").setValue(null);
+                form.getAttribute("ts_riskthreshold").setValue(null);
+                form.getControl("ts_operation").setDisabled(true);
+                form.getControl("ts_activitytype").setDisabled(true);
+                form.getControl("ts_riskthreshold").setDisabled(true);
+            }
+            else {
+                //Set Operation Field
+            }
+        }
+        SuggestedInspection.siteOnChange = siteOnChange;
+        function operationOnChange(eContext) {
+            var form = eContext.getFormContext();
+            var operationValue = form.getAttribute("ts_operation").getValue();
+            if (operationValue == null) {
+                //Clear and lock all dependent fields
+                form.getAttribute("ts_activitytype").setValue(null);
+                form.getAttribute("ts_riskthreshold").setValue(null);
+                form.getControl("ts_activitytype").setDisabled(true);
+                form.getControl("ts_riskthreshold").setDisabled(true);
+            }
+            else {
+                //Unlock next field
+                form.getControl("ts_activitytype").setDisabled(false);
+                setActivityTypeFilteredView(form);
+            }
+        }
+        SuggestedInspection.operationOnChange = operationOnChange;
+        function activityTypeOnChange(eContext) {
+            var form = eContext.getFormContext();
+            var activtyTypeValue = form.getAttribute("ts_activitytype").getValue();
+            if (activtyTypeValue == null) {
+                //Clear and lock all dependent fields
+                form.getAttribute("ts_riskthreshold").setValue(null);
+                form.getControl("ts_riskthreshold").setDisabled(true);
+            }
+        }
+        SuggestedInspection.activityTypeOnChange = activityTypeOnChange;
         function setOperationTypeFilteredView(form) {
             var viewId = '{8982C38D-8BB4-4C95-BD05-493398FEAE99}';
             var entityName = "ovs_operationtype";
-            var viewDisplayName = Xrm.Utility.getResourceString("ovs_/resx/WorkOrder", "FilteredOperationTypes");
+            //const viewDisplayName = Xrm.Utility.getResourceString("ovs_/resx/WorkOrder", "FilteredOperationTypes");
+            var viewDisplayName = "Operation Types";
             var fetchXml = [
                 "<fetch>",
                 "  <entity name='ovs_operationtype'>",
-                "    <link-entity name='ovs_operation' from='ovs_operationtypeid' to='ovs_operationtypeid' link-type='inner' alias='operation'>",
-                "      <link-entity name='businessunit' from='businessunitid' to='owningbusinessunit' link-type='inner' alias='businessunit'>",
-                "        <filter>",
-                "          <condition attribute='name' operator='begins-with' value='Intermodal'/>",
-                "        </filter>",
-                "      </link-entity>",
-                "      <link-entity name='msdyn_incidenttype' from='ts_ovs_operation' to='ovs_operationid' link-type='outer' alias='incidenttype'>",
+                "    <attribute name='createdon'/>",
+                "    <attribute name='ovs_name'/>",
+                "    <attribute name='ovs_operationtypeid'/>",
+                "    <link-entity name='businessunit' from='businessunitid' to='owningbusinessunit' alias='businessunit'>",
+                "      <filter>",
+                "        <condition attribute='name' operator='begins-with' value='Intermodal'/>",
+                "      </filter>",
+                "    </link-entity>",
+                "    <link-entity name='ts_ovs_operationtypes_msdyn_incidenttypes' from='ovs_operationtypeid' to='ovs_operationtypeid' intersect='true'>",
+                "      <link-entity name='msdyn_incidenttype' from='msdyn_incidenttypeid' to='msdyn_incidenttypeid' alias='incidenttype'>",
                 "        <filter>",
                 "          <condition attribute='msdyn_defaultworkordertype' operator='eq' value='b1ee680a-7cf7-ea11-a815-000d3af3a7a7' uiname='Inspection' uitype='msdyn_workordertype'/>",
                 "        </filter>",
@@ -32,6 +124,63 @@ var ROM;
             ].join("");
             var layoutXml = '<grid name="resultset" object="10010" jump="name" select="1" icon="1" preview="1"><row name="result" id="ovs_operationtypeid"><cell name="ovs_name" width="200" /></row></grid>';
             form.getControl("ts_operationtype").addCustomView(viewId, entityName, viewDisplayName, fetchXml, layoutXml, true);
+        }
+        function setStakeholderFilteredView(form) {
+            var operationTypeValue = form.getAttribute("ts_operationtype").getValue();
+            var operationTypeId;
+            if (operationTypeValue != null) {
+                operationTypeId = operationTypeValue[0].id;
+            }
+            var viewId = '{8982C38D-8BB4-4C95-BD05-493398FEAE98}';
+            var entityName = "account";
+            //const viewDisplayName = Xrm.Utility.getResourceString("ovs_/resx/WorkOrder", "FilteredStakeholders");
+            var viewDisplayName = "Stakeholders";
+            var fetchXml = [
+                "<fetch>",
+                "  <entity name='account'>",
+                "    <attribute name='accountid'/>",
+                "    <attribute name='createdon'/>",
+                "    <attribute name='name'/>",
+                "    <link-entity name='ovs_operation' from='ts_stakeholder' to='accountid'>",
+                "      <filter>",
+                "        <condition attribute='ovs_operationtypeid' operator='eq' value='", operationTypeId, "'uitype='ovs_operationtype'/>",
+                "      </filter>",
+                "    </link-entity>",
+                "  </entity>",
+                "</fetch>"
+            ].join("");
+            var layoutXml = '<grid name="resultset" object="10010" jump="name" select="1" icon="1" preview="1"><row name="result" id="accountid"><cell name="name" width="200" /></row></grid>';
+            form.getControl("ts_stakeholder").addCustomView(viewId, entityName, viewDisplayName, fetchXml, layoutXml, true);
+        }
+        function setSiteFilteredView(form) {
+            var operationTypeValue = form.getAttribute("ts_operationtype").getValue();
+            var stakeholderValue = form.getAttribute("ts_stakeholder").getValue();
+            var operationTypeId;
+            var stakeholderId;
+            if (operationTypeValue != null && stakeholderValue != null) {
+                operationTypeId = operationTypeValue[0].id;
+                stakeholderId = operationTypeValue[0].id;
+            }
+            var viewId = '{8982C38D-8BB4-4C95-BD05-493398FEAE98}';
+            var entityName = "account";
+            //const viewDisplayName = Xrm.Utility.getResourceString("ovs_/resx/WorkOrder", "FilteredStakeholders");
+            var viewDisplayName = "Stakeholders";
+            var fetchXml = [
+                "<fetch>",
+                "  <entity name='msdyn_functionallocation'>",
+                "    <link-entity name='ovs_operation' from='ts_site' to='msdyn_functionallocationid' alias='operation'>",
+                "      <filter>",
+                "        <condition attribute='ts_stakeholder' operator='eq' value='", stakeholderId, "' uitype='account'/>",
+                "        <condition attribute='ovs_operationtypeid' operator='eq' value='", operationTypeId, "' uitype='ovs_operationtype'/>",
+                "      </filter>",
+                "    </link-entity>",
+                "  </entity>",
+                "</fetch>"
+            ].join("");
+            var layoutXml = '<grid name="resultset" object="10010" jump="name" select="1" icon="1" preview="1"><row name="result" id="accountid"><cell name="name" width="200" /></row></grid>';
+            form.getControl("ts_stakeholder").addCustomView(viewId, entityName, viewDisplayName, fetchXml, layoutXml, true);
+        }
+        function setActivityTypeFilteredView(form) {
         }
     })(SuggestedInspection = ROM.SuggestedInspection || (ROM.SuggestedInspection = {}));
 })(ROM || (ROM = {}));
--- TSIS2.WebResources/src/ts/SuggestedInspection.ts
+++ TSIS2.WebResources/src/ts/SuggestedInspection.ts
@@ -1,23 +1,122 @@
 ﻿namespace ROM.SuggestedInspection {
     export function onLoad(eContext) {
         const form = <Form.ts_suggestedinspection.Main.Information>eContext.getFormContext();
-        setOperationTypeFilteredView(form)
+        setOperationTypeFilteredView(form);
+    }
+
+    export function operationTypeOnChange(eContext) {
+        const form = <Form.ts_suggestedinspection.Main.Information>eContext.getFormContext();
+
+        const operationTypeValue = form.getAttribute("ts_operationtype").getValue();
+        if (operationTypeValue == null) {
+            //Clear and lock all dependent fields
+            form.getAttribute("ts_stakeholder").setValue(null);
+            form.getAttribute("ts_site").setValue(null);
+            form.getAttribute("ts_operation").setValue(null);
+            form.getAttribute("ts_activitytype").setValue(null);
+            form.getAttribute("ts_riskthreshold").setValue(null);
+
+            form.getControl("ts_stakeholder").setDisabled(true);
+            form.getControl("ts_site").setDisabled(true);
+            form.getControl("ts_operation").setDisabled(true);
+            form.getControl("ts_activitytype").setDisabled(true);
+            form.getControl("ts_riskthreshold").setDisabled(true);
+        } else {
+            //Unlock next field
+            form.getControl("ts_stakeholder").setDisabled(false);
+            setStakeholderFilteredView(form);
+        }
+    }
+
+    export function stakeholderOnChange(eContext) {
+        const form = <Form.ts_suggestedinspection.Main.Information>eContext.getFormContext();
+
+        const stakeholderValue = form.getAttribute("ts_stakeholder").getValue();
+        if (stakeholderValue == null) {
+            //Clear and lock all dependent fields
+            form.getAttribute("ts_site").setValue(null);
+            form.getAttribute("ts_operation").setValue(null);
+            form.getAttribute("ts_activitytype").setValue(null);
+            form.getAttribute("ts_riskthreshold").setValue(null);
+
+            form.getControl("ts_site").setDisabled(true);
+            form.getControl("ts_operation").setDisabled(true);
+            form.getControl("ts_activitytype").setDisabled(true);
+            form.getControl("ts_riskthreshold").setDisabled(true);
+        } else {
+            //Unlock next field
+            form.getControl("ts_site").setDisabled(false);
+            setSiteFilteredView(form);
+        }
+    }
+
+    export function siteOnChange(eContext) {
+        const form = <Form.ts_suggestedinspection.Main.Information>eContext.getFormContext();
+
+        const siteValue = form.getAttribute("ts_site").getValue();
+        if (siteValue == null) {
+            //Clear and lock all dependent fields
+            form.getAttribute("ts_operation").setValue(null);
+            form.getAttribute("ts_activitytype").setValue(null);
+            form.getAttribute("ts_riskthreshold").setValue(null);
+
+            form.getControl("ts_operation").setDisabled(true);
+            form.getControl("ts_activitytype").setDisabled(true);
+            form.getControl("ts_riskthreshold").setDisabled(true);
+        } else {
+            //Set Operation Field
+
+        }
+    }
+
+    export function operationOnChange(eContext) {
+        const form = <Form.ts_suggestedinspection.Main.Information>eContext.getFormContext();
+
+        const operationValue = form.getAttribute("ts_operation").getValue();
+        if (operationValue == null) {
+            //Clear and lock all dependent fields
+            form.getAttribute("ts_activitytype").setValue(null);
+            form.getAttribute("ts_riskthreshold").setValue(null);
+
+            form.getControl("ts_activitytype").setDisabled(true);
+            form.getControl("ts_riskthreshold").setDisabled(true);
+        } else {
+            //Unlock next field
+            form.getControl("ts_activitytype").setDisabled(false);
+            setActivityTypeFilteredView(form);
+        }
+    }
+
+    export function activityTypeOnChange(eContext) {
+        const form = <Form.ts_suggestedinspection.Main.Information>eContext.getFormContext();
+
+        const activtyTypeValue = form.getAttribute("ts_activitytype").getValue();
+        if (activtyTypeValue == null) {
+            //Clear and lock all dependent fields
+            form.getAttribute("ts_riskthreshold").setValue(null);
+
+            form.getControl("ts_riskthreshold").setDisabled(true);
+        }
     }
 
     function setOperationTypeFilteredView(form: Form.ts_suggestedinspection.Main.Information): void {
         const viewId = '{8982C38D-8BB4-4C95-BD05-493398FEAE99}';
         const entityName = "ovs_operationtype";
-        const viewDisplayName = Xrm.Utility.getResourceString("ovs_/resx/WorkOrder", "FilteredOperationTypes");
+        //const viewDisplayName = Xrm.Utility.getResourceString("ovs_/resx/WorkOrder", "FilteredOperationTypes");
+        const viewDisplayName = "Operation Types";
         const fetchXml = [
             "<fetch>",
             "  <entity name='ovs_operationtype'>",
-            "    <link-entity name='ovs_operation' from='ovs_operationtypeid' to='ovs_operationtypeid' link-type='inner' alias='operation'>",
-            "      <link-entity name='businessunit' from='businessunitid' to='owningbusinessunit' link-type='inner' alias='businessunit'>",
-            "        <filter>",
-            "          <condition attribute='name' operator='begins-with' value='Intermodal'/>",
-            "        </filter>",
-            "      </link-entity>",
-            "      <link-entity name='msdyn_incidenttype' from='ts_ovs_operation' to='ovs_operationid' link-type='outer' alias='incidenttype'>",
+            "    <attribute name='createdon'/>",
+            "    <attribute name='ovs_name'/>",
+            "    <attribute name='ovs_operationtypeid'/>",
+            "    <link-entity name='businessunit' from='businessunitid' to='owningbusinessunit' alias='businessunit'>",
+            "      <filter>",
+            "        <condition attribute='name' operator='begins-with' value='Intermodal'/>",
+            "      </filter>",
+            "    </link-entity>",
+            "    <link-entity name='ts_ovs_operationtypes_msdyn_incidenttypes' from='ovs_operationtypeid' to='ovs_operationtypeid' intersect='true'>",
+            "      <link-entity name='msdyn_incidenttype' from='msdyn_incidenttypeid' to='msdyn_incidenttypeid' alias='incidenttype'>",
             "        <filter>",
             "          <condition attribute='msdyn_defaultworkordertype' operator='eq' value='b1ee680a-7cf7-ea11-a815-000d3af3a7a7' uiname='Inspection' uitype='msdyn_workordertype'/>",
             "        </filter>",
@@ -29,4 +128,67 @@
         const layoutXml = '<grid name="resultset" object="10010" jump="name" select="1" icon="1" preview="1"><row name="result" id="ovs_operationtypeid"><cell name="ovs_name" width="200" /></row></grid>';
         form.getControl("ts_operationtype").addCustomView(viewId, entityName, viewDisplayName, fetchXml, layoutXml, true);
     }
+
+    function setStakeholderFilteredView(form: Form.ts_suggestedinspection.Main.Information): void {
+        let operationTypeValue = form.getAttribute("ts_operationtype").getValue();
+        let operationTypeId;
+        if (operationTypeValue != null) {
+            operationTypeId = operationTypeValue[0].id;
+        }
+        const viewId = '{8982C38D-8BB4-4C95-BD05-493398FEAE98}';
+        const entityName = "account";
+        //const viewDisplayName = Xrm.Utility.getResourceString("ovs_/resx/WorkOrder", "FilteredStakeholders");
+        const viewDisplayName = "Stakeholders";
+        const fetchXml = [
+            "<fetch>",
+            "  <entity name='account'>",
+            "    <attribute name='accountid'/>",
+            "    <attribute name='createdon'/>",
+            "    <attribute name='name'/>",
+            "    <link-entity name='ovs_operation' from='ts_stakeholder' to='accountid'>",
+            "      <filter>",
+            "        <condition attribute='ovs_operationtypeid' operator='eq' value='", operationTypeId, "'uitype='ovs_operationtype'/>",
+            "      </filter>",
+            "    </link-entity>",
+            "  </entity>",
+            "</fetch>"
+        ].join("");
+        const layoutXml = '<grid name="resultset" object="10010" jump="name" select="1" icon="1" preview="1"><row name="result" id="accountid"><cell name="name" width="200" /></row></grid>';
+        form.getControl("ts_stakeholder").addCustomView(viewId, entityName, viewDisplayName, fetchXml, layoutXml, true);
+    }
+
+    function setSiteFilteredView(form: Form.ts_suggestedinspection.Main.Information): void {
+        let operationTypeValue = form.getAttribute("ts_operationtype").getValue();
+        let stakeholderValue = form.getAttribute("ts_stakeholder").getValue();
+        let operationTypeId;
+        let stakeholderId;
+        if (operationTypeValue != null && stakeholderValue != null) {
+            operationTypeId = operationTypeValue[0].id;
+            stakeholderId = operationTypeValue[0].id;
+        }
+        const viewId = '{8982C38D-8BB4-4C95-BD05-493398FEAE98}';
+        const entityName = "account";
+        //const viewDisplayName = Xrm.Utility.getResourceString("ovs_/resx/WorkOrder", "FilteredStakeholders");
+        const viewDisplayName = "Stakeholders";
+        const fetchXml = [
+            "<fetch>",
+            "  <entity name='msdyn_functionallocation'>",
+            "    <link-entity name='ovs_operation' from='ts_site' to='msdyn_functionallocationid' alias='operation'>",
+            "      <filter>",
+            "        <condition attribute='ts_stakeholder' operator='eq' value='", stakeholderId, "' uitype='account'/>",
+            "        <condition attribute='ovs_operationtypeid' operator='eq' value='", operationTypeId, "' uitype='ovs_operationtype'/>",
+            "      </filter>",
+            "    </link-entity>",
+            "  </entity>",
+            "</fetch>"
+        ].join("");
+        const layoutXml = '<grid name="resultset" object="10010" jump="name" select="1" icon="1" preview="1"><row name="result" id="accountid"><cell name="name" width="200" /></row></grid>';
+        form.getControl("ts_stakeholder").addCustomView(viewId, entityName, viewDisplayName, fetchXml, layoutXml, true);
+    }
+
+    function setActivityTypeFilteredView(form: Form.ts_suggestedinspection.Main.Information): void {
+
+    }
+
+
 }
