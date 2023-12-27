d204493bed882be7cc0fc2c2a1f05bb3ea45b6a6
DBurneyTC <Derek.Burney@tc.gc.ca>
2023-12-27 09:40:46 -0500

    create response function for selected subgrid

--- TSIS2.WebResources/Webresources/js/QuestionnaireVersionRibbon.js
+++ TSIS2.WebResources/Webresources/js/QuestionnaireVersionRibbon.js
@@ -29,3 +29,39 @@ function createResponse(primaryControl) {
         }
     );
 }
+
+function createResponseSelected(FirstSelectedItemId) {
+    Xrm.WebApi.retrieveRecord("ts_questionnaireversion", FirstSelectedItemId, "?$select=ts_questionnairedefinition").then(
+        function success(result) {
+            if (result.ts_questionnairedefinition == null) return;
+
+            var data =
+            {
+                "ts_questionnairedefinition": result.ts_questionnairedefinition.slice(1, -1)
+            }
+            // create account record
+            Xrm.WebApi.createRecord("ts_questionnaireresponse", data).then(
+                function success(result) {
+                    var pageInput = {
+                        pageType: "entityrecord",
+                        entityName: "ts_questionnaireresponse",
+                        entityId: result.id
+                    };
+                    //Open finding record
+                    Xrm.Navigation.navigateTo(pageInput).then(
+                        function success() {
+                            // Run code on success
+                        },
+                        function error() {
+                            // Handle errors
+                        }
+                    );
+                },
+                function (error) {
+                    console.log(error.message);
+                    // handle error conditions
+                }
+            );
+        }
+    );
+}
