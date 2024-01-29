9377e363b7f3dd2c71262375d26eb4fd48e7844b
DBurneyTC <Derek.Burney@tc.gc.ca>
2024-01-29 11:04:17 -0500

    create Operation Risk Assessment Ribbon file

---
+++ TSIS2.WebResources/Webresources/js/OperationRiskAssessmentRibbon.js
@@ -0,0 +1,5 @@
+﻿export function recalculateRiskScore(formContext) {
+    //For this to work Offline, we have to avoid using retrieveMultiple
+    //retrieveRecord using a guid will work, and the guids can be accessed from the subgrid on the form
+    //Retrieve all related Risk Criteria Responses, expand the choice with weight
+}
