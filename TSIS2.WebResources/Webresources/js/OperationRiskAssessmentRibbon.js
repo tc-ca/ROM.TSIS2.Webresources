23b4dd295264749bb14906f86a10cf421877ee8a
DBurneyTC <Derek.Burney@tc.gc.ca>
2024-01-30 16:07:24 -0500

    calculation log

--- TSIS2.WebResources/Webresources/js/OperationRiskAssessmentRibbon.js
+++ TSIS2.WebResources/Webresources/js/OperationRiskAssessmentRibbon.js
@@ -4,6 +4,7 @@ function recalculateRiskScore(formContext) {
     Xrm.Utility.showProgressIndicator();
     let RiskScoreSet = false;
     let DiscretionaryScoreSet = false;
+    let calculationLog = "";
 
     //Calculate and set Risk Score
     const RiskResponseGrid = formContext.getControl("Subgrid_Risk_Criteria_Responses");
@@ -11,8 +12,9 @@ function recalculateRiskScore(formContext) {
     const RiskCriteriaRetrievals = [];
     for (let i = 0; i < RiskResponseRows.getLength(); i++) {
         let RiskResponseRow = RiskResponseRows.get(i).getData();
-        RiskCriteriaRetrievals.push(Xrm.WebApi.retrieveRecord("ts_riskcriteriaresponse", RiskResponseRow._entity._entityId.guid, "?$select=ts_riskcriteriaoption&$expand=ts_riskcriteriaoption($select=ts_weight)").then(
+        RiskCriteriaRetrievals.push(Xrm.WebApi.retrieveRecord("ts_riskcriteriaresponse", RiskResponseRow._entity._entityId.guid, "?$select=ts_riskcriteriaoption,ts_name&$expand=ts_riskcriteriaoption($select=ts_weight,ts_name)").then(
             function success(result) {
+                calculationLog += `Risk Criteria "${result.ts_name}" with option "${result.ts_riskcriteriaoption.ts_name}" has a weight of ${result.ts_riskcriteriaoption.ts_weight} \n`;
                 if (result.ts_riskcriteriaoption.ts_weight != isNaN) {
                     return result.ts_riskcriteriaoption.ts_weight
                 }
@@ -22,10 +24,14 @@ function recalculateRiskScore(formContext) {
     Promise.all(RiskCriteriaRetrievals).then((weights) => {
         let totalWeight = weights.reduce((sum, ele) => sum + ele);
         formContext.getAttribute("ts_riskcriteriascore").setValue(totalWeight);
+        calculationLog += `Set Risk Criteria Score to ${totalWeight} \n`;
         RiskScoreSet = true;
         if (RiskScoreSet && DiscretionaryScoreSet) {
-            formContext.getAttribute("ts_riskscore").setValue(formContext.getAttribute("ts_discretionaryscore").getValue() + formContext.getAttribute("ts_riskcriteriascore").getValue());
+            const riskScore = formContext.getAttribute("ts_discretionaryscore").getValue() + formContext.getAttribute("ts_riskcriteriascore").getValue();
+            formContext.getAttribute("ts_riskscore").setValue(riskScore);
+            calculationLog += `Set Risk Score to ${riskScore} \n`;
             Xrm.Utility.closeProgressIndicator();
+            formContext.getAttribute("ts_calculationlog").setValue(calculationLog);
             formContext.data.entity.save();
         }
     });
@@ -36,9 +42,11 @@ function recalculateRiskScore(formContext) {
     const DiscretionaryRetrievals = [];
     for (let i = 0; i < DiscretionaryRows.getLength(); i++) {
         let DiscretionaryResponseRow = DiscretionaryRows.get(i).getData();
-        DiscretionaryRetrievals.push(Xrm.WebApi.retrieveRecord("ts_discretionaryfactorresponse", DiscretionaryResponseRow._entity._entityId.guid, "?$select=ts_discretionaryfactoroption&$expand=ts_discretionaryfactoroption($select=ts_weight),ts_discretionaryfactorgrouping($select=ts_scorerangeminimum,ts_scorerangemaximum)").then(
+        DiscretionaryRetrievals.push(Xrm.WebApi.retrieveRecord("ts_discretionaryfactorresponse", DiscretionaryResponseRow._entity._entityId.guid, "?$select=ts_discretionaryfactoroption,ts_name&$expand=ts_discretionaryfactoroption($select=ts_weight,ts_name),ts_discretionaryfactorgrouping($select=ts_scorerangeminimum,ts_scorerangemaximum, ts_name)").then(
             function success(result) {
+                calculationLog += `Discretionary Factor "${result.ts_name}" with option "${result.ts_discretionaryfactoroption.ts_name}" in group of "${result.ts_discretionaryfactorgrouping.ts_name}" (Min: ${result.ts_discretionaryfactorgrouping.ts_scorerangeminimum}, Max ${result.ts_discretionaryfactorgrouping.ts_scorerangemaximum}) has a weight of ${result.ts_discretionaryfactoroption.ts_weight} \n`;
                 return {
+                    groupingName: result.ts_discretionaryfactorgrouping.ts_name,
                     groupingId: result.ts_discretionaryfactorgrouping.ts_discretionaryfactorgroupingid,
                     groupingMin: result.ts_discretionaryfactorgrouping.ts_scorerangeminimum,
                     groupingMax: result.ts_discretionaryfactorgrouping.ts_scorerangemaximum,
@@ -54,7 +62,8 @@ function recalculateRiskScore(formContext) {
             if (groupingScores[discretionaryResponse.groupingId] == null) {
                 groupingScores[discretionaryResponse.groupingId] = {
                     min: discretionaryResponse.groupingMin,
-                    max: discretionaryResponse.groupingMax
+                    max: discretionaryResponse.groupingMax,
+                    name: discretionaryResponse.groupingName
                 }
             }
             if (groupingScores[discretionaryResponse.groupingId].score == null) {
@@ -62,23 +71,31 @@ function recalculateRiskScore(formContext) {
             } else {
                 groupingScores[discretionaryResponse.groupingId].score += discretionaryResponse.weight;
             }
+            calculationLog += `Adding ${discretionaryResponse.weight} to ${groupingScores[discretionaryResponse.groupingId].name} Group. Current Group Score: ${groupingScores[discretionaryResponse.groupingId].score} \n`;
         }
 
         //The score added for a group cannot be outside the min and max values
         for (let groupingId in groupingScores) {
-            if (groupingScores[groupingId].score < groupingScores[groupingId].min) {
+            if (groupingScores[groupingId].score <= groupingScores[groupingId].min) {
                 totalScore += groupingScores[groupingId].min;
-            } else if (groupingScores[groupingId].score < groupingScores[groupingId].max) {
+                calculationLog += `${groupingScores[groupingId].name} Group score of ${groupingScores[groupingId].score} is less than or equal to group minimum of ${groupingScores[groupingId].min}. Adding ${groupingScores[groupingId].min}. Current Discretionary Score Total: ${totalScore} \n`;
+            } else if (groupingScores[groupingId].score <= groupingScores[groupingId].max) {
                 totalScore += groupingScores[groupingId].score;
+                calculationLog += `${groupingScores[groupingId].name} Group score of ${groupingScores[groupingId].score} is less than or equal to group maximum of ${groupingScores[groupingId].max}. Adding ${groupingScores[groupingId].score}. Current Discretionary Score Total: ${totalScore} \n`;
             } else {
                 totalScore += groupingScores[groupingId].max;
+                calculationLog += `${groupingScores[groupingId].name} Group score of ${groupingScores[groupingId].score} is above group maximum of ${groupingScores[groupingId].max}. Adding ${groupingScores[groupingId].max}. Current Discretionary Score Total: ${totalScore} \n`;
             }
         }
         formContext.getAttribute("ts_discretionaryscore").setValue(totalScore);
+        calculationLog += `Set Discretionary Score to ${totalScore} \n`;
         DiscretionaryScoreSet = true;
         if (RiskScoreSet && DiscretionaryScoreSet) {
-            formContext.getAttribute("ts_riskscore").setValue(formContext.getAttribute("ts_discretionaryscore").getValue() + formContext.getAttribute("ts_riskcriteriascore").getValue());
+            const riskScore = formContext.getAttribute("ts_discretionaryscore").getValue() + formContext.getAttribute("ts_riskcriteriascore").getValue();
+            formContext.getAttribute("ts_riskscore").setValue(riskScore);
+            calculationLog += `Set Risk Score to ${riskScore} \n`;
             Xrm.Utility.closeProgressIndicator();
+            formContext.getAttribute("ts_calculationlog").setValue(calculationLog);
             formContext.data.entity.save();
         }
     });
