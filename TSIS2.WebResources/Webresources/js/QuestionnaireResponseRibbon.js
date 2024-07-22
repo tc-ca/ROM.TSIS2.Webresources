ab692f51c26a806782e622ff42c1af547201ef27
Elena0925 <59444477+Elena0925@users.noreply.github.com>
2024-07-22 13:52:50 -0400

    Filtered WO Values

--- TSIS2.WebResources/Webresources/js/QuestionnaireResponseRibbon.js
+++ TSIS2.WebResources/Webresources/js/QuestionnaireResponseRibbon.js
@@ -2,12 +2,21 @@ var lang = parent.Xrm.Utility.getGlobalContext().userSettings.languageId;
 
 function appendToWOST(formContext) {
     const questionnaireResponseGuid = formContext.data.entity.getId().replace(/({|})/g, '').toLowerCase();
+    const userId = Xrm.Utility.getGlobalContext().userSettings.userId.replace(/({|})/g, '').toLowerCase();
+    var jsonData = {
+        recordId: questionnaireResponseGuid,
+        userId: userId,
+        isAdminOrManager: isAdminOrManager()
+    };
+    var jsonString = JSON.stringify(jsonData).toString();
     // Centered Dialog
     var pageInput = {
         pageType: "custom",
         name: "ts_appendquestionnaireresponsetoservicetask_6789e", //Unique name of Custom page
-        recordId: questionnaireResponseGuid
+        recordId: jsonString
+           
     };
+
     var navigationOptions = {
         target: 2,
         position: 1,
@@ -26,4 +35,16 @@ function appendToWOST(formContext) {
             // Handle error
         }
     );
+}
+
+function isAdminOrManager(){
+    const userRoles = Xrm.Utility.getGlobalContext().userSettings.roles;
+    //If the user is a system admin or ROM - Manager, show the RATE manager review section
+    let isAdminOrManager = false;
+    userRoles.forEach(role => {
+        if (role.name == "System Administrator" || role.name == "ROM - Manager") {
+            isAdminOrManager = true;
+        }
+    });
+    return isAdminOrManager;
 }
