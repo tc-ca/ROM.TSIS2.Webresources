02f27868a78115cdb83f88c2e9e4d4efc0857ff9
Dong Hua <dong.hua@tc.gc.ca>
2024-08-23 09:18:03 -0400

    PBI AB#363930

--- TSIS2.WebResources/Webresources/js/IncidentType.js
+++ TSIS2.WebResources/Webresources/js/IncidentType.js
@@ -79,6 +79,8 @@ var ROM;
                     Xrm.WebApi.retrieveMultipleRecords("businessunit", businessUnitfetchXml).then(function (result) {
                         if (result.entities[0].name.startsWith("Aviation")) {
                             form.ui.tabs.get("operation_activity_tab").setVisible(true);
+                            form.getControl("ts_programarea").setVisible(true);
+                            form.getControl("ts_programactivityriskrating").setVisible(true);
                         }
                     });
                 });
