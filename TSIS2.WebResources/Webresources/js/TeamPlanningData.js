a42d6a558d5ba21c4c0c570aca77f776d8b13893
Elena Maslakova <elena.maslakova@tc.gc.ca>
2024-10-16 08:17:44 -0400

    VSIs created during flows

--- TSIS2.WebResources/Webresources/js/TeamPlanningData.js
+++ TSIS2.WebResources/Webresources/js/TeamPlanningData.js
@@ -14,7 +14,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
             if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
             if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
@@ -46,7 +46,7 @@ var ROM;
                 formContext.getControl("ts_team").setDisabled(true);
                 formContext.getControl("ts_fiscalyear").setDisabled(true);
                 var planStatusValue = formContext.getAttribute("ts_planstatus").getValue();
-                if (planStatusValue == 741130001 /* Complete */ || planStatusValue == 447390001 /* HQreview */) {
+                if (planStatusValue == 741130001 /* ts_planstatus.Complete */ || planStatusValue == 447390001 /* ts_planstatus.HQreview */) {
                     if (userHasRole("System Administrator|ROM - Business Admin")) {
                         formContext.getControl("ts_planstatus").setDisabled(false);
                     }
@@ -520,7 +520,7 @@ var ROM;
         function planStatusOnChange(eContext) {
             var formContext = eContext.getFormContext();
             var planStatusValue = formContext.getAttribute("ts_planstatus").getValue();
-            if (planStatusValue == 741130001 /* Complete */ || planStatusValue == 447390001 /* HQreview */) {
+            if (planStatusValue == 741130001 /* ts_planstatus.Complete */ || planStatusValue == 447390001 /* ts_planstatus.HQreview */) {
                 formContext.getControl("ts_totalhoursq1").setDisabled(true);
                 formContext.getControl("ts_totalhoursq2").setDisabled(true);
                 formContext.getControl("ts_totalhoursq3").setDisabled(true);
