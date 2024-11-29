1eb29c432d0e88ffe19304fcc5d10fb367b9fb69
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2024-11-29 16:58:56 -0500

    Implement onLoad function to set entity attributes based on parent record details
    - Extract parent record ID, type, and name from query parameters
    - Set entity name, ID, and name attributes based on parent record type
    - Handle cases where parent record information is missing

--- TSIS2.WebResources/Webresources/js/EntityRisk.js
+++ TSIS2.WebResources/Webresources/js/EntityRisk.js
@@ -4,12 +4,59 @@ var ROM;
     var EntityRisk;
     (function (EntityRisk) {
         function onLoad(eContext) {
-            var form = eContext.getFormContext();
+            var formContext = eContext.getFormContext();
             console.log("Entering EntityRisk onLoad");
+            // if we are on the new form
+            if (formContext.ui.getFormType() == 1) {
+                var globalContext = Xrm.Utility.getGlobalContext();
+                var queryParams = globalContext.getQueryStringParameters();
+                // Extract the id and record type of the parent form this is calling from
+                var parentRecordId = queryParams["parentrecordid"];
+                var parentRecordType = queryParams["parentrecordtype"];
+                var parentRecordName = queryParams["parentrecordname"];
+                if (parentRecordType != null && parentRecordId != null && parentRecordName != null) {
+                    // Set the Entity Name choice
+                    switch (parentRecordType) {
+                        case "account":
+                            formContext.getAttribute("ts_entityname").setValue(741130004 /* Stakeholder */);
+                            break;
+                        case "msdyn_functionallocation":
+                            formContext.getAttribute("ts_entityname").setValue(741130003 /* Site */);
+                            break;
+                        case "ovs_operationtype":
+                            formContext.getAttribute("ts_entityname").setValue(741130002 /* OperationType */);
+                            break;
+                        case "ovs_operation":
+                            formContext.getAttribute("ts_entityname").setValue(741130001 /* Operation */);
+                            break;
+                        case "msdyn_incidenttype":
+                            formContext.getAttribute("ts_entityname").setValue(741130005 /* ActivityType */);
+                            break;
+                        case "ts_programarea":
+                            formContext.getAttribute("ts_entityname").setValue(741130000 /* ProgramArea */);
+                            break;
+                        default:
+                            console.log("Unknown Parent Record Type");
+                            formContext.getAttribute("ts_entityname").setValue(null);
+                            break;
+                    }
+                    // Set the Entity ID
+                    formContext.getAttribute("ts_entityid").setValue(parentRecordId);
+                    // Set the Name
+                    formContext.getAttribute("ts_name").setValue(parentRecordName);
+                }
+                else {
+                    console.log("ERROR unable to retrieve any parent record information");
+                    // These are read-only or hidden fields on the form.  If we can't get any parent record information then the record can't be created since they are required.
+                    formContext.getAttribute("ts_entityname").setValue(null);
+                    formContext.getAttribute("ts_entityid").setValue(parentRecordId);
+                    formContext.getAttribute("ts_name").setValue(parentRecordName);
+                }
+            }
         }
         EntityRisk.onLoad = onLoad;
         function onSave(eContext) {
-            var form = eContext.getFormContext();
+            var formContext = eContext.getFormContext();
             console.log("Entering EntityRisk onSave");
         }
         EntityRisk.onSave = onSave;
