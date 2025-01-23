a0729a507e4284c1927d820a7eda37a1ac8c5fe3
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2025-01-23 15:54:35 -0500

    Hookup Javascript for the OperationType form

---
+++ TSIS2.WebResources/src/ts/OperationType.ts
@@ -0,0 +1,9 @@
+﻿namespace ROM.OperationType {
+    export async function onLoad(eContext: Xrm.ExecutionContext<any, any>) {
+        console.log("onLoad working");
+    }
+    export function onSave(eContext: Xrm.ExecutionContext<any, any>): void {
+        console.log("onSave working");
+    }
+
+}
