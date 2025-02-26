60c0568136668263a6cee404d459f08fe086751e
Elena Maslakova <elena.maslakova@tc.gc.ca>
2025-02-26 14:16:10 -0500

    On Operation Activity Form, lock all fields from editing by all roles except business admin

--- TSIS2.WebResources/src/ts/OperationActivity.ts
+++ TSIS2.WebResources/src/ts/OperationActivity.ts
@@ -5,20 +5,24 @@ namespace ROM.OperationActivity {
         const operationAttribute = form.getAttribute("ts_operation");
         const activityTypeAttribute = form.getAttribute("ts_activity");
 
-        if(operationAttribute != null && operationAttribute != undefined && activityTypeAttribute != null && activityTypeAttribute != undefined){
-            const operationAttributeValue= operationAttribute.getValue();
-            const activityTypeAttributeValue= activityTypeAttribute.getValue();
+        if (operationAttribute != null && operationAttribute != undefined && activityTypeAttribute != null && activityTypeAttribute != undefined) {
+            const operationAttributeValue = operationAttribute.getValue();
+            const activityTypeAttributeValue = activityTypeAttribute.getValue();
 
-            if(operationAttributeValue != null && activityTypeAttributeValue != null){
+            if (operationAttributeValue != null && activityTypeAttributeValue != null) {
                 form.ui.tabs.get("related_wos_tab").setVisible(true);
 
                 let fetchXml = `<filter><condition attribute="msdyn_primaryincidenttype" operator="eq" value="${activityTypeAttributeValue[0].id}"/><condition attribute="ovs_operationid" operator="eq" value="${operationAttributeValue[0].id}"/><condition attribute="statecode" operator="eq" value="0" /></filter>`;
 
-                if(form.ui.getFormType() != 0 && form.ui.getFormType() != 1 && form.ui.getFormType() != 6){
+                if (form.ui.getFormType() != 0 && form.ui.getFormType() != 1 && form.ui.getFormType() != 6) {
                     //setRelatedWorkOrdersFetchXML(form, fetchXml)
                 }
             }
         }
+        //If not business admin lock all fields
+        if (!isAdmin()) {
+            setAllFieldsDisabled(eContext);
+        }            
     }
 
     export function setRelatedWorkOrdersFetchXML(form: Form.ts_operationactivity.Main.Information, fetchXml: string) {
@@ -28,8 +32,28 @@ namespace ROM.OperationActivity {
             setTimeout(ROM.OperationActivity.setRelatedWorkOrdersFetchXML, 1000);
             return;
         }
-        else{
+        else {
             ROM.Utils.setSubgridFilterXml(form, "related_wos", fetchXml);
         }
+    }
+
+    function setAllFieldsDisabled(eContext) {
+        const formContext = eContext.getFormContext();
+
+        formContext.ui.controls.forEach(function (control, i) {
+            if (control && control.getDisabled && !control.getDisabled()) {
+                control.setDisabled!(true);
+            }
+        });
+    }
+    function isAdmin() {
+        const userRoles = Xrm.Utility.getGlobalContext().userSettings.roles;
+        let isAdmin = false;
+        userRoles.forEach(role => {
+            if (role.name == "System Administrator" || role.name == "ROM - Business Admin") {
+                isAdmin = true;
+            }
+        });
+        return isAdmin;
     }
 }
