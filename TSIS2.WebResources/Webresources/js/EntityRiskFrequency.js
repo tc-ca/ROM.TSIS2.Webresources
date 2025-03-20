de8fe44816d12af28caefd4b5e4cd00f7bfbb76b
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2025-03-20 15:30:24 -0400

    Initial setup for Entity Risk Frequency

---
+++ TSIS2.WebResources/Webresources/js/EntityRiskFrequency.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+var ROM;
+(function (ROM) {
+    var EntityRiskFrequency;
+    (function (EntityRiskFrequency) {
+        function onLoad(eContext) {
+            var formContext = eContext.getFormContext();
+            console.log("Entering onLoad");
+        }
+        EntityRiskFrequency.onLoad = onLoad;
+        function onSave(eContext) {
+            var formContext = eContext.getFormContext();
+            console.log("Entering onSave");
+        }
+        EntityRiskFrequency.onSave = onSave;
+    })(EntityRiskFrequency = ROM.EntityRiskFrequency || (ROM.EntityRiskFrequency = {}));
+})(ROM || (ROM = {}));
