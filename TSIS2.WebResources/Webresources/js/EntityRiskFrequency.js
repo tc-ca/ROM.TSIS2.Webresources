f2a90c710cdf1054afb235952ff93df8f082f0cb
rpetrietc <94928365+rpetrietc@users.noreply.github.com>
2025-03-21 14:49:33 -0400

    Add unique key generation  `generateUniqueKey(formContext)` function, which generates a unique key based on the selected fiscal year and entity during form save.
    Set initial required levels for entity selection attributes to "none" and conditionally update to "required" based on user selection in `entityNameOnChange`.

--- TSIS2.WebResources/Webresources/js/EntityRiskFrequency.js
+++ TSIS2.WebResources/Webresources/js/EntityRiskFrequency.js
@@ -23,6 +23,7 @@ var ROM;
         EntityRiskFrequency.onLoad = onLoad;
         function onSave(eContext) {
             var formContext = eContext.getFormContext();
+            generateUniqueKey(formContext);
             console.log("Entering onSave");
         }
         EntityRiskFrequency.onSave = onSave;
@@ -34,9 +35,16 @@ var ROM;
             if (selectedEntity === null)
                 return;
             // Show the specific lookup based on the entity selected
+            formContext.getAttribute("ts_activitytype").setRequiredLevel("none");
+            formContext.getAttribute("ts_operation").setRequiredLevel("none");
+            formContext.getAttribute("ts_operationtype").setRequiredLevel("none");
+            formContext.getAttribute("ts_programarea").setRequiredLevel("none");
+            formContext.getAttribute("ts_site").setRequiredLevel("none");
+            formContext.getAttribute("ts_stakeholder").setRequiredLevel("none");
             switch (selectedEntity) {
                 case 741130000:
                     console.log("Activity Type selected");
+                    formContext.getAttribute("ts_activitytype").setRequiredLevel("required");
                     formContext.getControl("ts_activitytype").setVisible(true);
                     formContext.getControl("ts_operation").setVisible(false);
                     formContext.getControl("ts_operationtype").setVisible(false);
@@ -46,6 +54,7 @@ var ROM;
                     break;
                 case 741130001:
                     console.log("Operation selected");
+                    formContext.getAttribute("ts_operation").setRequiredLevel("required");
                     formContext.getControl("ts_activitytype").setVisible(false);
                     formContext.getControl("ts_operation").setVisible(true);
                     formContext.getControl("ts_operationtype").setVisible(false);
@@ -55,6 +64,7 @@ var ROM;
                     break;
                 case 741130002:
                     console.log("Operation Type selected");
+                    formContext.getAttribute("ts_operationtype").setRequiredLevel("required");
                     formContext.getControl("ts_activitytype").setVisible(false);
                     formContext.getControl("ts_operation").setVisible(false);
                     formContext.getControl("ts_operationtype").setVisible(true);
@@ -64,6 +74,7 @@ var ROM;
                     break;
                 case 741130003:
                     console.log("Program Area selected");
+                    formContext.getAttribute("ts_programarea").setRequiredLevel("required");
                     formContext.getControl("ts_activitytype").setVisible(false);
                     formContext.getControl("ts_operation").setVisible(false);
                     formContext.getControl("ts_operationtype").setVisible(false);
@@ -73,6 +84,7 @@ var ROM;
                     break;
                 case 741130004:
                     console.log("Site selected");
+                    formContext.getAttribute("ts_site").setRequiredLevel("required");
                     formContext.getControl("ts_activitytype").setVisible(false);
                     formContext.getControl("ts_operation").setVisible(false);
                     formContext.getControl("ts_operationtype").setVisible(false);
@@ -82,6 +94,7 @@ var ROM;
                     break;
                 case 741130005:
                     console.log("Stakeholder selected");
+                    formContext.getAttribute("ts_stakeholder").setRequiredLevel("required");
                     formContext.getControl("ts_activitytype").setVisible(false);
                     formContext.getControl("ts_operation").setVisible(false);
                     formContext.getControl("ts_operationtype").setVisible(false);
@@ -106,6 +119,67 @@ var ROM;
             var fetchXml = "<fetch version=\"1.0\" mapping=\"logical\" distinct=\"true\" returntotalrecordcount=\"true\" page=\"1\" count=\"25\" no-lock=\"false\">\n                            <entity name=\"tc_tcfiscalyear\">\n                              <attribute name=\"tc_tcfiscalyearid\" />\n                              <attribute name=\"tc_name\" />\n                              <order attribute=\"tc_fiscalyearnum\" descending=\"false\" />\n                              <filter>\n                                <condition attribute=\"tc_fiscalyearnum\" operator=\"ge\" value=\"" + yearsAgo + "\" />\n                                <condition attribute=\"tc_fiscalyearnum\" operator=\"le\" value=\"" + yearsFromNow + "\" />\n                              </filter>\n                            </entity>\n                          </fetch>";
             var layoutXml = '<grid name="resultset" object="10010" jump="tc_name" select="1" icon="1" preview="1"><row name="result" id="tc_tcfiscalyearid"><cell name="tc_name" width="200" /></row></grid>';
             formContext.getControl("ts_fiscalyear").addCustomView(viewId, entityName, viewDisplayName, fetchXml, layoutXml, true);
+        }
+        function generateUniqueKey(formContext) {
+            var generatedUniqueKey = "";
+            var fiscalYear = formContext.getAttribute("ts_fiscalyear").getValue();
+            var selectedEntity = formContext.getAttribute("ts_entityname").getValue();
+            // Ensure fiscalYear and entityName are not null
+            if (fiscalYear === null || selectedEntity === null)
+                return;
+            var fiscalYearName = fiscalYear[0].name;
+            var entityGuid = "";
+            switch (selectedEntity) {
+                case 741130000:
+                    console.log("Activity Type selected");
+                    var activityType = formContext.getAttribute("ts_activitytype").getValue();
+                    if (activityType != null) {
+                        entityGuid = activityType[0].id;
+                    }
+                    break;
+                case 741130001:
+                    console.log("Operation selected");
+                    var operation = formContext.getAttribute("ts_operation").getValue();
+                    if (operation != null) {
+                        entityGuid = operation[0].id;
+                    }
+                    break;
+                case 741130002:
+                    console.log("Operation Type selected");
+                    var operationType = formContext.getAttribute("ts_operationtype").getValue();
+                    if (operationType != null) {
+                        entityGuid = operationType[0].id;
+                    }
+                    break;
+                case 741130003:
+                    console.log("Program Area selected");
+                    var programArea = formContext.getAttribute("ts_programarea").getValue();
+                    if (programArea != null) {
+                        entityGuid = programArea[0].id;
+                    }
+                    break;
+                case 741130004:
+                    console.log("Site selected");
+                    var site = formContext.getAttribute("ts_site").getValue();
+                    if (site != null) {
+                        entityGuid = site[0].id;
+                    }
+                    break;
+                case 741130005:
+                    console.log("Stakeholder selected");
+                    var stakeholder = formContext.getAttribute("ts_stakeholder").getValue();
+                    if (stakeholder != null) {
+                        entityGuid = stakeholder[0].id;
+                    }
+                    break;
+                default:
+                    console.log("Entity Name drop down selection - Unknown selection");
+                    // Optional: Handle cases where the selection is not recognized
+                    break;
+            }
+            // Generate the unique key
+            generatedUniqueKey = fiscalYearName + ":" + entityGuid;
+            formContext.getAttribute("ts_generateduniquekey").setValue(generatedUniqueKey);
         }
     })(EntityRiskFrequency = ROM.EntityRiskFrequency || (ROM.EntityRiskFrequency = {}));
 })(ROM || (ROM = {}));
--- TSIS2.WebResources/src/ts/EntityRiskFrequency.ts
+++ TSIS2.WebResources/src/ts/EntityRiskFrequency.ts
@@ -23,6 +23,9 @@
 
     export function onSave(eContext: Xrm.ExecutionContext<any, any>): void {
         const formContext = <Form.ts_entityriskfrequency.Main.Information>eContext.getFormContext();
+
+        generateUniqueKey(formContext);
+
         console.log("Entering onSave");
     }
 
@@ -37,9 +40,18 @@
 
         // Show the specific lookup based on the entity selected
 
+        formContext.getAttribute("ts_activitytype").setRequiredLevel("none");
+        formContext.getAttribute("ts_operation").setRequiredLevel("none");
+        formContext.getAttribute("ts_operationtype").setRequiredLevel("none");
+        formContext.getAttribute("ts_programarea").setRequiredLevel("none");
+        formContext.getAttribute("ts_site").setRequiredLevel("none");
+        formContext.getAttribute("ts_stakeholder").setRequiredLevel("none");
+
         switch (selectedEntity) {
             case 741130000:
                 console.log("Activity Type selected");
+
+                formContext.getAttribute("ts_activitytype").setRequiredLevel("required");
 
                 formContext.getControl("ts_activitytype").setVisible(true);
                 formContext.getControl("ts_operation").setVisible(false);
@@ -53,6 +65,8 @@
             case 741130001:
                 console.log("Operation selected");
 
+                formContext.getAttribute("ts_operation").setRequiredLevel("required");
+
                 formContext.getControl("ts_activitytype").setVisible(false);
                 formContext.getControl("ts_operation").setVisible(true);
                 formContext.getControl("ts_operationtype").setVisible(false);
@@ -64,6 +78,8 @@
 
             case 741130002:
                 console.log("Operation Type selected");
+
+                formContext.getAttribute("ts_operationtype").setRequiredLevel("required");
 
                 formContext.getControl("ts_activitytype").setVisible(false);
                 formContext.getControl("ts_operation").setVisible(false);
@@ -77,6 +93,8 @@
             case 741130003:
                 console.log("Program Area selected");
 
+                formContext.getAttribute("ts_programarea").setRequiredLevel("required");
+
                 formContext.getControl("ts_activitytype").setVisible(false);
                 formContext.getControl("ts_operation").setVisible(false);
                 formContext.getControl("ts_operationtype").setVisible(false);
@@ -89,6 +107,8 @@
             case 741130004:
                 console.log("Site selected");
 
+                formContext.getAttribute("ts_site").setRequiredLevel("required");
+
                 formContext.getControl("ts_activitytype").setVisible(false);
                 formContext.getControl("ts_operation").setVisible(false);
                 formContext.getControl("ts_operationtype").setVisible(false);
@@ -100,6 +120,8 @@
 
             case 741130005:
                 console.log("Stakeholder selected");
+
+                formContext.getAttribute("ts_stakeholder").setRequiredLevel("required");
 
                 formContext.getControl("ts_activitytype").setVisible(false);
                 formContext.getControl("ts_operation").setVisible(false);
@@ -142,5 +164,96 @@
         const layoutXml = '<grid name="resultset" object="10010" jump="tc_name" select="1" icon="1" preview="1"><row name="result" id="tc_tcfiscalyearid"><cell name="tc_name" width="200" /></row></grid>';
 
         formContext.getControl("ts_fiscalyear").addCustomView(viewId, entityName, viewDisplayName, fetchXml, layoutXml, true);
+    }
+
+    function generateUniqueKey(formContext: Form.ts_entityriskfrequency.Main.Information) {
+
+        let generatedUniqueKey = "";
+        let fiscalYear = formContext.getAttribute("ts_fiscalyear").getValue();
+        let selectedEntity = formContext.getAttribute("ts_entityname").getValue();
+
+        // Ensure fiscalYear and entityName are not null
+        if (fiscalYear === null || selectedEntity === null) return;
+
+        let fiscalYearName = fiscalYear[0].name;
+        let entityGuid = "";
+
+        switch (selectedEntity) {
+            case 741130000:
+                console.log("Activity Type selected");
+
+                let activityType = formContext.getAttribute("ts_activitytype").getValue();
+
+                if (activityType != null) {
+                    entityGuid = activityType[0].id;
+                } 
+
+                break;
+
+            case 741130001:
+                console.log("Operation selected");
+
+                let operation = formContext.getAttribute("ts_operation").getValue();
+
+                if (operation != null) {
+                    entityGuid = operation[0].id;
+                } 
+
+                break;
+
+            case 741130002:
+                console.log("Operation Type selected");
+
+                let operationType = formContext.getAttribute("ts_operationtype").getValue();
+
+                if (operationType != null) {
+                    entityGuid = operationType[0].id;
+                }
+
+                break;
+
+            case 741130003:
+                console.log("Program Area selected");
+
+                let programArea = formContext.getAttribute("ts_programarea").getValue();
+
+                if (programArea != null) {
+                    entityGuid = programArea[0].id;
+                }
+
+                break;
+
+            case 741130004:
+                console.log("Site selected");
+
+                let site = formContext.getAttribute("ts_site").getValue();
+
+                if (site != null) {
+                    entityGuid = site[0].id;
+                }
+
+                break;
+
+            case 741130005:
+                console.log("Stakeholder selected");
+
+                let stakeholder = formContext.getAttribute("ts_stakeholder").getValue();
+
+                if (stakeholder != null) {
+                    entityGuid = stakeholder[0].id;
+                }
+
+                break;
+
+            default:
+                console.log("Entity Name drop down selection - Unknown selection");
+                // Optional: Handle cases where the selection is not recognized
+                break;
+
+        }
+
+        // Generate the unique key
+        generatedUniqueKey = fiscalYearName + ":" + entityGuid;
+        formContext.getAttribute("ts_generateduniquekey").setValue(generatedUniqueKey);
     }
 }
