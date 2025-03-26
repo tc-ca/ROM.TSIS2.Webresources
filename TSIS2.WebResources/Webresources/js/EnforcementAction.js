c34e92cc8c45174f65b978e963bad788b59c9934
Elena Maslakova <elena.maslakova@tc.gc.ca>
2025-03-26 08:49:21 -0400

    Add accountable Team to Operation Activity - AvSec

--- TSIS2.WebResources/Webresources/js/EnforcementAction.js
+++ TSIS2.WebResources/Webresources/js/EnforcementAction.js
@@ -107,7 +107,7 @@ var ROM;
         EnforcementAction.typeOnChange = typeOnChange;
         function additionalDetailsVisibility(formContext) {
             var typeAttributeValue = formContext.getAttribute("ts_typeofenforcementaction").getValue();
-            if (typeAttributeValue != null && typeAttributeValue == 717750000 /* VerbalWarning */) {
+            if (typeAttributeValue != null && typeAttributeValue == 717750000 /* ts_type.VerbalWarning */) {
                 formContext.ui.tabs.get("general").sections.get("additional_details").setVisible(true);
                 formContext.getControl("ts_verbalwarninggivento").setVisible(true);
                 formContext.getControl("ts_individualposition").setVisible(true);
@@ -124,7 +124,7 @@ var ROM;
                 formContext.getAttribute("ts_writtenwarningsentto").setRequiredLevel("none");
                 formContext.getAttribute("ts_writtenwarningdeliverymethod").setRequiredLevel("none");
             }
-            else if (typeAttributeValue != null && typeAttributeValue == 717750001 /* WrittenWarning */) {
+            else if (typeAttributeValue != null && typeAttributeValue == 717750001 /* ts_type.WrittenWarning */) {
                 formContext.ui.tabs.get("general").sections.get("additional_details").setVisible(true);
                 formContext.getControl("ts_writtenwarningsentto").setVisible(true);
                 formContext.getControl("ts_individualposition").setVisible(true);
--- TSIS2.WebResources/Webresources/js/FunctionalLocation.js
+++ TSIS2.WebResources/Webresources/js/FunctionalLocation.js
@@ -99,12 +99,12 @@ var ROM;
             var statusEndDateValue = form.getAttribute("ts_statusenddate").getValue();
             if (statusStartDateValue != null) {
                 if (Date.parse(statusStartDateValue.toDateString()) <= Date.parse(new Date(Date.now()).toDateString())) {
-                    form.getAttribute("ts_sitestatus").setValue(717750001 /* NonOperational */);
+                    form.getAttribute("ts_sitestatus").setValue(717750001 /* ts_sitestatus.NonOperational */);
                 }
             }
             if (statusEndDateValue != null) {
                 if (Date.parse(statusEndDateValue.toDateString()) <= Date.parse(new Date(Date.now()).toDateString())) {
-                    form.getAttribute("ts_sitestatus").setValue(717750000 /* Operational */);
+                    form.getAttribute("ts_sitestatus").setValue(717750000 /* ts_sitestatus.Operational */);
                 }
             }
         }
@@ -206,7 +206,7 @@ var ROM;
         //Shows the Risk Score field only when the Class is 2 or 3
         function riskScoreVisibility(form) {
             var siteClass = form.getAttribute("ts_class").getValue();
-            if (siteClass == 717750002 /* _2 */ || siteClass == 717750003 /* _3 */) {
+            if (siteClass == 717750002 /* ts_msdyn_functionallocation_ts_class._2 */ || siteClass == 717750003 /* ts_msdyn_functionallocation_ts_class._3 */) {
                 form.getControl("ts_riskscore").setVisible(true);
                 form.getControl("ts_lpdtounitedstates").setVisible(true);
             }
