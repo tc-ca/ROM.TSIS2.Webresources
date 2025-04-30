bd4b13c4f8adf848913268f91330d475dab0ec4b
H <hamza.etth@gmail.com>
2025-04-30 09:48:45 -0400

    Corrected visibility logic for French risk threshold field

--- TSIS2.WebResources/Webresources/js/OperationRiskAssessment.js
+++ TSIS2.WebResources/Webresources/js/OperationRiskAssessment.js
@@ -7,7 +7,7 @@ var ROM;
             var form = eContext.getFormContext();
             var riskThresholdFrench = form.getControl("ts_riskthresholdfrench");
             var riskThresholdEnglish = form.getControl("ts_riskthresholdenglish");
-            Xrm.Utility.getGlobalContext().userSettings.languageId == 1033 ? riskThresholdEnglish.setVisible(true) : riskThresholdFrench.setVisible(false);
+            Xrm.Utility.getGlobalContext().userSettings.languageId == 1033 ? riskThresholdEnglish.setVisible(true) : riskThresholdFrench.setVisible(true);
         }
         OperationRiskAssessment.onLoad = onLoad;
     })(OperationRiskAssessment = ROM.OperationRiskAssessment || (ROM.OperationRiskAssessment = {}));
