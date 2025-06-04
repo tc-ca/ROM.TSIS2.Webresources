8b934c4cd6b423fcc0113dbdadba44f394b64bd7
Mistry, Aakash (TC TC) <aakash.mistry@tc.gc.ca>
2025-06-04 10:36:13 -0400

    Creation of PrescribedFrequencyOverride.ts

---
+++ TSIS2.WebResources/Webresources/js/PrescribedFrequencyOverride.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+var ROM;
+(function (ROM) {
+    var PrescribedFrequencyOverride;
+    (function (PrescribedFrequencyOverride) {
+        function onLoad(eContext) {
+            var formContext = eContext.getFormContext();
+            console.log("Entering onLoad");
+        }
+        PrescribedFrequencyOverride.onLoad = onLoad;
+    })(PrescribedFrequencyOverride = ROM.PrescribedFrequencyOverride || (ROM.PrescribedFrequencyOverride = {}));
+})(ROM || (ROM = {}));
---
+++ TSIS2.WebResources/src/ts/PrescribedFrequencyOverride.ts
@@ -0,0 +1,8 @@
+﻿namespace ROM.PrescribedFrequencyOverride {
+    export function onLoad(eContext: Xrm.ExecutionContext<any, any>): void {
+        const formContext = <Form.ts_prescribedfrequencyoverride.Main.Information>eContext.getFormContext();
+        console.log("Entering onLoad");
+
+
+    }
+}
