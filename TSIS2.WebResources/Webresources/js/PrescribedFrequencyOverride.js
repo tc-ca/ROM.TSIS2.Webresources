dd3dc4e8e466304a9dc4652728e2901cf943f560
Mistry, Aakash (TC TC) <aakash.mistry@tc.gc.ca>
2025-06-06 16:08:33 -0400

    Update English and French Name when fiscal year or activity type is changed

--- TSIS2.WebResources/Webresources/js/PrescribedFrequencyOverride.js
+++ TSIS2.WebResources/Webresources/js/PrescribedFrequencyOverride.js
@@ -6,6 +6,8 @@ var ROM;
         function fiscalYearOnChange(eContext) {
             var _a;
             var formContext = eContext.getFormContext();
+            // This function is used to retrieve the selected lookup value and set the English and French names based on the selected activity type and fiscal year
+            setEnglishandFrenchName(formContext);
             // Get values from the form
             var activityTypeLookup = (_a = formContext.getAttribute("ts_activitytype")) === null || _a === void 0 ? void 0 : _a.getValue();
             var activityTypeId = "";
@@ -55,5 +57,83 @@ var ROM;
             });
         }
         PrescribedFrequencyOverride.fiscalYearOnChange = fiscalYearOnChange;
+        function activityTypeOnChange(eContext) {
+            var formContext = eContext.getFormContext();
+            // This function is used to retrieve the selected lookup value and set the English and French names based on the selected activity type and fiscal year
+            setEnglishandFrenchName(formContext);
+        }
+        PrescribedFrequencyOverride.activityTypeOnChange = activityTypeOnChange;
+        // This function is used to retrieve the English and French display names for the selected entity and set the ts_englishname and ts_frenchname fields along with the Fiscal Year
+        function setEnglishandFrenchName(formContext) {
+            // get the selection from ts_entityname
+            var selectedEntity = formContext.getAttribute("ts_activitytype").getValue();
+            // Ensure selectedEntity is not null
+            if (selectedEntity === null)
+                return;
+            // get the user's language ID
+            var userLanguageId = Xrm.Utility.getGlobalContext().userSettings.languageId;
+            var fetchXml = "";
+            var selectedTable = "";
+            var encodedFetchXml = "";
+            if (selectedEntity && selectedEntity.length > 0) {
+                selectedTable = "msdyn_incidenttype";
+                // Fetch the selected activity type record
+                fetchXml = "<fetch>\n                              <entity name=\"msdyn_incidenttype\">\n                                <attribute name=\"ovs_incidenttypenameenglish\" />\n                                <attribute name=\"ovs_incidenttypenamefrench\" />\n                                <filter>\n                                  <condition attribute=\"msdyn_incidenttypeid\" operator=\"eq\" value=\"" + selectedEntity[0].id + "\" />\n                                </filter>\n                              </entity>\n                            </fetch>";
+            }
+            // make sure the fetchXml is not empty
+            if (fetchXml === "")
+                return;
+            encodedFetchXml = "?fetchXml=" + encodeURIComponent(fetchXml);
+            Xrm.WebApi.retrieveMultipleRecords(selectedTable, encodedFetchXml).then(function (result) {
+                if (result.entities.length > 0) {
+                    console.log("Record found");
+                    var record = result.entities[0];
+                    var displayNameEnglish_1 = "";
+                    var displayNameFrench_1 = "";
+                    if (selectedEntity) {
+                        displayNameEnglish_1 = record["ovs_incidenttypenameenglish"];
+                        displayNameFrench_1 = record["ovs_incidenttypenamefrench"];
+                    }
+                    console.log("Retrieved English display name:", displayNameEnglish_1);
+                    console.log("Retrieved French display name:", displayNameFrench_1);
+                    // Make sure displayNameEnglish is not empty
+                    if (displayNameEnglish_1 === "")
+                        return;
+                    // Get the selected Fiscal Year
+                    var fiscalYear = formContext.getAttribute("ts_fiscalyear").getValue();
+                    // Ensure fiscalYear is not null
+                    if (fiscalYear === null)
+                        return;
+                    // Get the Fiscal Year
+                    fetchXml = "<fetch>\n                              <entity name=\"tc_tcfiscalyear\">\n                                <attribute name=\"tc_fiscalyearlonglbl\" />\n                                <filter>\n                                  <condition attribute=\"tc_tcfiscalyearid\" operator=\"eq\" value=\"" + fiscalYear[0].id + "\" />\n                                </filter>\n                              </entity>\n                            </fetch>";
+                    encodedFetchXml = "?fetchXml=" + encodeURIComponent(fetchXml);
+                    Xrm.WebApi.retrieveMultipleRecords("tc_tcfiscalyear", encodedFetchXml).then(function (result) {
+                        if (result.entities.length > 0) {
+                            console.log("Record found");
+                            var record_1 = result.entities[0];
+                            var fiscalYear_1 = record_1["tc_fiscalyearlonglbl"];
+                            console.log("Retrieved fiscal year:", fiscalYear_1);
+                            // if the fiscal year is not null
+                            if (fiscalYear_1 !== null) {
+                                // Set the English display name
+                                formContext.getAttribute("ts_englishname").setValue(displayNameEnglish_1 + " - " + fiscalYear_1);
+                                // Set the French display name
+                                formContext.getAttribute("ts_frenchname").setValue(displayNameFrench_1 + " - " + fiscalYear_1);
+                            }
+                        }
+                        else {
+                            console.log("Record not found");
+                        }
+                    }, function (error) {
+                        console.error("Error retrieving record:", error.message);
+                    });
+                }
+                else {
+                    console.log("Record not found");
+                }
+            }, function (error) {
+                console.error("Error retrieving record:", error.message);
+            });
+        }
     })(PrescribedFrequencyOverride = ROM.PrescribedFrequencyOverride || (ROM.PrescribedFrequencyOverride = {}));
 })(ROM || (ROM = {}));
--- TSIS2.WebResources/src/ts/PrescribedFrequencyOverride.ts
+++ TSIS2.WebResources/src/ts/PrescribedFrequencyOverride.ts
@@ -2,6 +2,9 @@
     export function fiscalYearOnChange(eContext: Xrm.ExecutionContext<any, any>): void {
         const formContext = <Form.ts_prescribedfrequencyoverride.Main.Information>eContext.getFormContext();
 
+        // This function is used to retrieve the selected lookup value and set the English and French names based on the selected activity type and fiscal year
+        setEnglishandFrenchName(formContext);
+
         // Get values from the form
         const activityTypeLookup = formContext.getAttribute("ts_activitytype")?.getValue();
         let activityTypeId = "";
@@ -24,7 +27,7 @@
             console.warn("Fiscal Year is not set.");
         }
 
-        
+
 
         // Fetch existing records to check for duplicates
         const fetchXml = `
@@ -77,5 +80,130 @@
         );
 
 
+    }
+
+    export function activityTypeOnChange(eContext: Xrm.ExecutionContext<any, any>): void {
+        const formContext = <Form.ts_prescribedfrequencyoverride.Main.Information>eContext.getFormContext();
+
+        // This function is used to retrieve the selected lookup value and set the English and French names based on the selected activity type and fiscal year
+        setEnglishandFrenchName(formContext);
+
+    }
+
+    // This function is used to retrieve the English and French display names for the selected entity and set the ts_englishname and ts_frenchname fields along with the Fiscal Year
+    function setEnglishandFrenchName(formContext: Form.ts_prescribedfrequencyoverride.Main.Information) {
+
+        // get the selection from ts_entityname
+        let selectedEntity = formContext.getAttribute("ts_activitytype").getValue();
+
+        // Ensure selectedEntity is not null
+        if (selectedEntity === null) return;
+
+        // get the user's language ID
+        const userLanguageId = Xrm.Utility.getGlobalContext().userSettings.languageId;
+
+        let fetchXml = "";
+        let selectedTable = "";
+        let encodedFetchXml = "";
+
+        if (selectedEntity && selectedEntity.length > 0) {
+
+            selectedTable = "msdyn_incidenttype";
+
+            // Fetch the selected activity type record
+            fetchXml = `<fetch>
+                              <entity name="msdyn_incidenttype">
+                                <attribute name="ovs_incidenttypenameenglish" />
+                                <attribute name="ovs_incidenttypenamefrench" />
+                                <filter>
+                                  <condition attribute="msdyn_incidenttypeid" operator="eq" value="${selectedEntity[0].id}" />
+                                </filter>
+                              </entity>
+                            </fetch>`;
+        }
+
+        // make sure the fetchXml is not empty
+        if (fetchXml === "") return;
+
+        encodedFetchXml = "?fetchXml=" + encodeURIComponent(fetchXml);
+
+        Xrm.WebApi.retrieveMultipleRecords(selectedTable, encodedFetchXml).then(
+            function (result) {
+                if (result.entities.length > 0) {
+                    console.log("Record found");
+
+                    const record = result.entities[0];
+
+                    let displayNameEnglish = "";
+                    let displayNameFrench = "";
+
+                    if (selectedEntity) {
+                        displayNameEnglish = record["ovs_incidenttypenameenglish"];
+                        displayNameFrench = record["ovs_incidenttypenamefrench"];
+
+                    }
+
+                    console.log("Retrieved English display name:", displayNameEnglish);
+                    console.log("Retrieved French display name:", displayNameFrench);
+
+                    // Make sure displayNameEnglish is not empty
+                    if (displayNameEnglish === "") return;
+
+                    // Get the selected Fiscal Year
+                    let fiscalYear = formContext.getAttribute("ts_fiscalyear").getValue();
+
+                    // Ensure fiscalYear is not null
+                    if (fiscalYear === null) return;
+
+                    // Get the Fiscal Year
+                    fetchXml = `<fetch>
+                              <entity name="tc_tcfiscalyear">
+                                <attribute name="tc_fiscalyearlonglbl" />
+                                <filter>
+                                  <condition attribute="tc_tcfiscalyearid" operator="eq" value="${fiscalYear[0].id}" />
+                                </filter>
+                              </entity>
+                            </fetch>`;
+
+                    encodedFetchXml = "?fetchXml=" + encodeURIComponent(fetchXml);
+
+                    Xrm.WebApi.retrieveMultipleRecords("tc_tcfiscalyear", encodedFetchXml).then(
+                        function (result) {
+                            if (result.entities.length > 0) {
+                                console.log("Record found");
+
+                                const record = result.entities[0];
+
+                                let fiscalYear = record["tc_fiscalyearlonglbl"];
+
+                                console.log("Retrieved fiscal year:", fiscalYear);
+
+                                // if the fiscal year is not null
+                                if (fiscalYear !== null) {
+
+                                    // Set the English display name
+                                    formContext.getAttribute("ts_englishname").setValue(displayNameEnglish + " - " + fiscalYear);
+
+                                    // Set the French display name
+                                    formContext.getAttribute("ts_frenchname").setValue(displayNameFrench + " - " + fiscalYear);
+                                }
+
+                            } else {
+                                console.log("Record not found");
+                            }
+                        },
+                        function (error) {
+                            console.error("Error retrieving record:", error.message);
+                        }
+                    );
+
+                } else {
+                    console.log("Record not found");
+                }
+            },
+            function (error) {
+                console.error("Error retrieving record:", error.message);
+            }
+        );
     }
 }
