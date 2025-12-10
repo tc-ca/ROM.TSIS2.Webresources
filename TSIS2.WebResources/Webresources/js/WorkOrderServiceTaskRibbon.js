f299bb7bb565312fdff22cef6db30cb68251c5e4
Khalid Hafid <khalid.hafid@tc.gc.ca>
2025-12-10 12:53:56 -0500

    missed this

--- TSIS2.WebResources/Webresources/js/WorkOrderServiceTaskRibbon.js
+++ TSIS2.WebResources/Webresources/js/WorkOrderServiceTaskRibbon.js
@@ -64,7 +64,7 @@ function isROMRole() {
 
 function isSystemAdministrator() {
   var roles = Xrm.Utility.getGlobalContext().userSettings.roles;
-  var enable = true;
+  var enable = false;
   roles.forEach(function (item) {
     if (item.name == "System Administrator" || isUserInTeamByEnvVar(TEAM_SCHEMA_NAMES.ROM_RAIL_SAFETY_ADMINISTRATOR)) enable = true;
   });
