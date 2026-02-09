591d554b2ee7bc31fb20842bf1c2d2b2645423e2
Mistry, Aakash (TC TC) <aakash.mistry@tc.gc.ca>
2026-02-09 12:08:58 -0500

    Open Action in modal window from subgrid

--- TSIS2.WebResources/Webresources/js/ActionRibbon.js
+++ TSIS2.WebResources/Webresources/js/ActionRibbon.js
@@ -1,1 +1,46 @@
-﻿
+﻿//Action for mscrm.OpenRecordItem overridden command. Opens Action forms as a modal.
+function openRecord(recordId) {
+    let formId = "8b389e0e-5f8c-44fe-afa8-d814de41eede"; //Information Main form
+    //Retrieve selected finding record
+    Xrm.WebApi.retrieveRecord("ts_action", recordId).then(
+        function (result) {
+            var pageInput = {
+                pageType: "entityrecord",
+                entityName: "ts_action",
+                entityId: recordId,
+            };
+            var navigationOptions = {
+                target: 2,
+                height: {
+                    value: 100,
+                    unit: "%",
+                },
+                width: {
+                    value: 80,
+                    unit: "%",
+                },
+                position: 1,
+            };
+            //Open finding record
+            Xrm.Navigation.navigateTo(pageInput, navigationOptions).then(
+                function success() {
+                    // Run code on success
+                },
+                function error() {
+                    // Handle errors
+                }
+            );
+        },
+        function (error) {
+            //If anything goes wrong log the error
+            console.log(error);
+
+            //and open "by default" form
+            Xrm.Navigation.openForm({
+                entityName: "ts_action",
+                entityId: recordId,
+                formId: formId,
+            });
+        }
+    );
+}
